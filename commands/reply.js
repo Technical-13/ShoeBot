af0677ce52ee0026084dcee43f54ddbb1ce20cd4
Donald J. Fortier II <Technical_13@yahoo.com>
2023-02-28 18:36:56 +0000

    Updated `/reply` && `/say` to log to console and send server owner a message when used.

--- commands/reply.js
+++ commands/reply.js
@@ -7,12 +7,23 @@ module.exports = {
     const { channel, options } = interaction;
     const msgID = interaction.options.getString( 'message-id' );
     const myResponse = interaction.options.getString( 'response' );
+    const strAuthorTag = interaction.user.tag;
+    const objGuildMembers = interaction.guild.members.cache;
+    const objGuildOwner = objGuildMembers.get( interaction.guild.ownerId );
 
-    channel.messages.fetch( msgID ).then( message => {
-      message.reply( myResponse );
-      interaction.editReply( { content: 'Responded!' } );
+    channel.messages.fetch( msgID ).then( async message => {
+      await message.reply( myResponse ).then( responded => {
+        interaction.editReply( { content: 'Responded!' } );
+        objGuildOwner.send( '<@' + interaction.user.id + '> requested me to reply to <@' + message.author.id + '> in ' + message.guild.name + '<#' + message.channel.id + '>:\n```\n' + myResponse + '\n```' );
+        console.log( '%o requested me to reply to %o in %o#%o:\n\t%o',
+          strAuthorTag, message.author.tag, message.guild.name, message.channel.name, myResponse
+        );
+      } );
     } ).catch( noMessage => {
       interaction.editReply( { content: 'Unable to find message to repyly to.' } );
+      console.log( '%o requested me to reply to a message I couldn\'t find (#%o):\n\t%o',
+        strAuthorTag, msgID, myResponse
+      );
     } );
 	}
 }
--- commands/say.js
+++ commands/say.js
@@ -6,22 +6,30 @@ module.exports = {
     interaction.deferReply( { ephemeral: true } );
     const speakChannel = interaction.options.getChannel( 'channel' ) || interaction.channel;
     const mySaying = interaction.options.getString( 'saying' );
+    const strAuthorTag = interaction.user.tag;
+    const objGuildMembers = interaction.guild.members.cache;
+    const objGuildOwner = objGuildMembers.get( interaction.guild.ownerId );
 
     if ( mySaying ) {      
       return speakChannel.send( mySaying ).then( spoke => {
-          interaction.editReply( { content: 'Said the thing!' } );                    } ).catch( muted => {
-          switch ( muted.code ) {
-            case 50001 :
-              let noChan = '<#' + speakChannel + '>';
-              interaction.editReply( {
-                content: 'I do not have permission to send messages in ' + noChan
-              } );
-              break;
-            default:
-              console.error( 'Unable to speak: %o', muted );
-              interaction.editReply( { content: 'Couldn\'t say the thing!' } );
-          }
-        } );
+        interaction.editReply( { content: 'Said the thing!' } );
+        objGuildOwner.send( '<@' + interaction.user.id + '> requested me to speak in ' + spoke.guild.name + '<#' + spoke.channel.id + '>:\n```\n' + mySaying + '\n```' );
+        console.log( '%o requested me to speak in %o#%o:\n\t%o',
+          strAuthorTag, spoke.guild.name, spoke.channel.name, mySaying
+        );
+      } ).catch( muted => {
+        switch ( muted.code ) {
+          case 50001 :
+            let noChan = '<#' + speakChannel + '>';
+            interaction.editReply( {
+              content: 'I do not have permission to send messages in ' + noChan
+            } );
+            break;
+          default:
+            console.error( 'Unable to speak: %o', muted );
+            interaction.editReply( { content: 'Couldn\'t say the thing!' } );
+        }
+      } );
     }
     console.error( 'mySaying: %o', mySaying );
     interaction.editReply( { content: 'I don\'t know what to say. :(' } );
