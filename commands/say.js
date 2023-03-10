9b7f61851c01b6e69f66be0257c8b26274f53217
Donald J. Fortier II <Technical_13@yahoo.com>
2023-03-10 13:37:50 +0000

    Updated to be able to DM bot owner from commands when there's an unknown error.

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,7 @@ module.exports = {
 	cooldown: 1000,
 	async run( interaction, client ) {
     interaction.deferReply( { ephemeral: true } );
+    const myOwner = client.users.cache.get( process.env.OWNERID );
     const speakChannel = interaction.options.getChannel( 'channel' ) || interaction.channel;
     const mySaying = interaction.options.getString( 'saying' );
     const strAuthorTag = interaction.user.tag;
@@ -11,27 +12,30 @@ module.exports = {
     const objGuildOwner = objGuildMembers.get( interaction.guild.ownerId );
 
     if ( mySaying ) {      
-      return speakChannel.send( mySaying ).then( spoke => {
-        interaction.editReply( { content: 'Said the thing!' } );
-        objGuildOwner.send( '<@' + interaction.user.id + '> requested me to speak in `' + spoke.guild.name + '`<#' + spoke.channel.id + '>:\n```\n' + mySaying + '\n```' );
-        console.log( '%o requested me to speak in %o#%o:\n\t%o',
-          strAuthorTag, spoke.guild.name, spoke.channel.name, mySaying
-        );
-      } ).catch( muted => {
+      await speakChannel.send( mySaying ).then( async spoke => {
+        await interaction.editReply( { content: 'I said the thing!' } );
+        await objGuildOwner.send( '<@' + interaction.user.id + '> requested me to speak in `' + spoke.guild.name + '`<#' + spoke.channel.id + '>:\n```\n' + mySaying + '\n```' );
+      } ).catch( async muted => {
         switch ( muted.code ) {
           case 50001 :
-            let noChan = '<#' + speakChannel + '>';
-            interaction.editReply( {
-              content: 'I do not have permission to send messages in ' + noChan
-            } );
+            const noChan = '<#' + speakChannel + '>';
+            await objGuildOwner.send( 'Please give me permission to send to ' + noChan );
+            await interaction.editReply( { content: 'I do not have permission to send messages in ' + noChan } );
             break;
           default:
-            console.error( 'Unable to speak: %o', muted );
-            interaction.editReply( { content: 'Couldn\'t say the thing!' } );
+            myOwner.send( 'Error attempting to speak as requested by: <@' + interaction.user.id + '>' +
+              ' from `' + interaction.guild.name + '`<#' + interaction.channel.id + '>:\n```\n' + muted + '\n```')
+              .then( notified => {
+                interaction.editReply( { content: 'Unknown error speaking. My owner, <@' + myOwner.id + '>, has been notified.' } );
+              } ).catch( notNotified => {
+                interaction.editReply( { content: 'Unknown error speaking. Unable to notify my owner, <@' + myOwner.id + '>.' } );
+              } );
+            console.error( 'Unable to speak:\n\tCode: %o\n\tMsg: %o\n\tErr: %o', muted.code, muted.message, muted );
         }
       } );
+    } else {
+      console.error( 'mySaying: %o', mySaying );
+      interaction.editReply( { content: 'I don\'t know what to say.' } );
     }
-    console.error( 'mySaying: %o', mySaying );
-    interaction.editReply( { content: 'I don\'t know what to say. :(' } );
 	}
 }
