8d616fd68c93f664f9a6f34ce6f34f5ef658ba24
Donald J. Fortier II <Technical_13@yahoo.com>
2024-09-23 09:34:28 -0400

    Cleanup and add some logging color.

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const Discord = require( 'discord.js' );
+const chalk = require( 'chalk' );
 const mongoose = require( 'mongoose' );
 
 module.exports = {
@@ -9,15 +10,15 @@ module.exports = {
     
     mongoose.set( 'strictQuery', false );
     client.user.setActivity( 'with bot code', { type: 'PLAYING' } );
-    mongoose.disconnect( () => console.log( 'Closed MongoDBs.' ) );
+    mongoose.disconnect( () => console.log( chalk.yellow( 'MongoDB closed.' ) ) );
     await mongoose.connect( process.env.mongodb || '', { keepAlive: true } )
-      .then( connected => { console.log( 'Connected to MongoDB.' ); } )
+      .then( connected => { console.log( chalk.green( 'Connected to MongoDB.' ) ); } )
       .catch( errDB => {
         myOwner.send( 'Failed to connect to MongoDB:\n```\n' + errDB + '\n```' )
-          .catch( errSend => { console.error( 'Unable to send DM:\n%o', errSend ); } );
-        console.error( 'Failed to connect to MongoDB:\n%o', errDB );
+          .catch( errSend => { console.error( chalk.red( `Unable to send DM:\n${errSend}` ) ); } );
+        console.error( chalk.red( `Failed to connect to MongoDB:\n${errDB}` ) );
       } );
     
-    console.log( 'Successfully logged in as: ' + client.user.tag );
+    console.log( chalk.green( `Successfully logged in as: ${client.user.tag}` ) ) );
 	}
 }
