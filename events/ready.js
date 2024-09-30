ee00e1175e2896f87aa78e51d96b04d9dfc7083f
Donald J. Fortier II <Technical_13@yahoo.com>
2024-09-30 19:37:24 -0400

    Update ready.js
    Currently live version.

--- events/ready.js
+++ events/ready.js
@@ -1,24 +1,27 @@
-const Discord = require( 'discord.js' );
+const client = require( '..' );
 const chalk = require( 'chalk' );
 const mongoose = require( 'mongoose' );
 
-module.exports = {
-	name: 'ready',
-	once: true,
-	async run( client ) {
-    const myOwner = client.users.cache.get( process.env.OWNERID );
+client.on( 'ready', async rdy => {
+    const today = ( new Date() );
+    const objTimeString = {"hour":"numeric","minute":"numeric","second":"numeric","timeZone":"America/New_York","timeZoneName":"short"};
+    const botTime = today.toLocaleTimeString( 'en-US', objTimeString );
+    console.log( chalk.bold( `The bot owner's local time is ${botTime}.` ) );
+    const hour = parseInt( botTime.split( ':' )[ 0 ] );
+    const myTime = ( hour >= 5 && hour < 12 ? 'morning' : ( hour >= 12 && hour < 18 ? 'evening' : 'nighttime' ) );
+    const myCup = ( hour >= 5 && hour < 12 ? 'my ' : ( hour >= 12 && hour < 18 ? 'an ' : 'a ' ) ) + myTime;
+    const activityTypes = { 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5 };
+    client.user.setPresence( { activities: [ { type: activityTypes.custom, name: '🥱 Just waking up...' } ], status: 'dnd' } );
+    setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.watching, name: 'my ' + myTime + ' coffee brew...' } ], status: 'dnd' } ); }, 15000 );
+    setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.custom, name: 'Drinking ' + myCup + ' cup of ☕' } ], status: 'idle' } ); }, 60000 );
+    setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.custom, name: 'https://discord.me/MagentaRV' } ], status: 'online' } ); }, 180000 );
     
+    const strConnectDB = ( process.env.mongodb || '' );
     mongoose.set( 'strictQuery', false );
-    client.user.setActivity( 'with bot code', { type: 'PLAYING' } );
-    mongoose.disconnect( () => console.log( chalk.yellow( 'MongoDB closed.' ) ) );
-    await mongoose.connect( process.env.mongodb || '', { keepAlive: true } )
-      .then( connected => { console.log( chalk.greenBright( 'Connected to MongoDB.' ) ); } )
-      .catch( errDB => {
-        myOwner.send( 'Failed to connect to MongoDB:\n```\n' + errDB + '\n```' )
-          .catch( errSend => { console.error( chalk.red( `Unable to send DM:\n${errSend}` ) ); } );
-        console.error( chalk.red( `Failed to connect to MongoDB:\n${errDB}` ) );
-      } );
-    
-    console.log( chalk.greenBright( `Successfully logged in as: ${client.user.tag}` ) );
-	}
-}
+    await mongoose.disconnect().then( dbDisconnected => console.log( chalk.yellow( 'MongoDB closed.' ) ) );
+    await mongoose.connect( strConnectDB )
+        .then( dbConnected => { console.log( chalk.greenBright( 'Connected to MongoDB.' ) ); } )
+        .catch( dbConnectErr => { console.error( chalk.bold.red( `Failed to connect to MongoDB:\n${dbConnectErr}` ) ); } );
+
+    console.log( chalk.bold.magentaBright( `Successfully logged in as: ${client.user.tag}` ) );
+} );
