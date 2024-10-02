f34c38dd23d010f3c715040e90209d11ee954ccc
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-02 09:20:00 -0400

    Add some responses

--- commands/admin/presence.js
+++ commands/admin/presence.js
@@ -1,49 +1,58 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, EmbedBuilder } = require( 'discord.js' );
 
 module.exports = {
-    name: 'presence',
-    description: 'Change the bot\s presence',
-    ownerOnly: true,
-    cooldown: 1000,
-    run: async ( client, message, args ) => {
-        const { author, channel, guild } = message;
-        const bot = client.user;
+  name: 'presence',
+  description: 'Change the bot\s presence',
+  ownerOnly: true,
+  cooldown: 1000,
+  run: async ( client, message, args ) => {
+    const { author, channel, guild } = message;
+    const bot = client.user;
 
-        message.delete();
+    message.delete();
 
-        const activityTypes = { 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5 };
-        const embedPresence = new EmbedBuilder()
-        .setTitle( 'setPresence' )
-        .setDescription( 'Set my activity, description, and status.' )
-        .setColor( '#FF00FF' )
-        .setTimestamp()
-        .setThumbnail( bot.displayAvatarURL() )
-        .setFooter( { text: bot.tag } )
+    const activityTypes = { 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5 };
+    const embedPresence = new EmbedBuilder()
+    .setTitle( 'setPresence' )
+    .setDescription( 'Set my activity, description, and status.' )
+    .setColor( '#FF00FF' )
+    .setTimestamp()
+    .setThumbnail( bot.displayAvatarURL() )
+    .setFooter( { text: bot.tag } )
 
-        const statusButtons = new ActionRowBuilder().addComponents(
-            new ButtonBuilder().setLabel( 'Offline' ).setCustomId( 'offline' ).setStyle( ButtonStyle.Secondary ),
-            new ButtonBuilder().setLabel( 'Do Not Disturb' ).setCustomId( 'dnd' ).setStyle( ButtonStyle.Danger ),
-            new ButtonBuilder().setLabel( 'Idle' ).setCustomId( 'idle' ).setStyle( ButtonStyle.Primary ),
-            new ButtonBuilder().setLabel( 'Online' ).setCustomId( 'online' ).setStyle( ButtonStyle.Success ) );
-        const embedInterface = await message.reply( { embeds: [ embedPresence ], components: [ statusButtons ] } );
-        const clickFilter = ( clicker ) => { clicker.user.id === author.id };
-        const buttonClicks = embedInterface.createMessageComponentCollector( { componentType: ComponentType.Button, clickFilter, maxProcessed: 1 } );
+    const statusButtons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder().setLabel( 'Offline' ).setCustomId( 'offline' ).setStyle( ButtonStyle.Secondary ),
+      new ButtonBuilder().setLabel( 'Do Not Disturb' ).setCustomId( 'dnd' ).setStyle( ButtonStyle.Danger ),
+      new ButtonBuilder().setLabel( 'Idle' ).setCustomId( 'idle' ).setStyle( ButtonStyle.Primary ),
+      new ButtonBuilder().setLabel( 'Online' ).setCustomId( 'online' ).setStyle( ButtonStyle.Success ) );
+    const embedInterface = await message.reply( { embeds: [ embedPresence ], components: [ statusButtons ] } );
+    const clickFilter = ( clicker ) => { clicker.user.id === author.id };
+    const buttonClicks = embedInterface.createMessageComponentCollector( { componentType: ComponentType.Button, clickFilter, maxProcessed: 1 } );
 
-        buttonClicks.on( 'collect', interaction => {
-            switch ( interaction.customId ) {
-                case 'offline':
-                    bot.setPresence( { status: 'offline' } );break;
-                case 'dnd':
-                    bot.setPresence( { status: 'dnd' } );break;
-                case 'idle':
-                    bot.setPresence( { status: 'idle' } );break;
-                case 'online': default:
-                    bot.setPresence( { status: 'online' } );
-            }
-        } );
+    buttonClicks.on( 'collect', interaction => {
+      switch ( interaction.customId ) {
+        case 'offline':
+          bot.setPresence( { status: 'offline' } );
+          interaction.reply( 'I am now **`Offline`**!' );
+          break;
+        case 'dnd':
+          bot.setPresence( { status: 'dnd' } );
+          interaction.reply( 'I am now **`Do Not Disturb`**!' );
+          
+          break;
+        case 'idle':
+          bot.setPresence( { status: 'idle' } );
+          interaction.reply( 'I am now **`Idle`**!' );
+          
+          break;
+        case 'online': default:
+          bot.setPresence( { status: 'online' } );
+          interaction.reply( 'I am now **`Online`**!' );
+      }
+    } );
 
-        buttonClicks.on( 'end', () => {
-            setTimeout( () => { embedInterface.delete(); }, 3000 );
-        } );
-    }
+    buttonClicks.on( 'end', () => {
+      setTimeout( () => { embedInterface.delete(); }, 3000 );
+    } );
+  }
 };
