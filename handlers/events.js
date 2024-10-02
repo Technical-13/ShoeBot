76ed6a5216f827f33f0a751458974ac43105cf90
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-02 07:40:21 -0400

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -1,13 +1,12 @@
 const fs = require( 'fs' );
-const chalk = require( 'chalk' )
-var AsciiTable = require( 'ascii-table' )
-var table = new AsciiTable()
-table.setHeading( 'Events', 'Stats' ).setBorder( '|', '=', "0", "0" )
+const chalk = require( 'chalk' );
+var AsciiTable = require( 'ascii-table' );
 
 module.exports = ( client ) => {
+    const table = new AsciiTable().setHeading( 'Events', 'Loaded' ).setBorder( '|', '=', "0", "0" ).setAlignCenter( 1 );
     fs.readdirSync( './events/' ).filter( ( file ) => file.endsWith( '.js' ) ).forEach( ( event ) => {
-      	require( `../events/${event}` );
-	table.addRow( event.split( '.js' )[ 0 ], '✅' )
-    } )
-	console.log( chalk.greenBright( table.toString() ) )
+        require( `../events/${event}` );
+        table.addRow( event.split( '.js' )[ 0 ], '✅' );
+    } );
+    console.log( chalk.greenBright( table.toString() ) );
 };
