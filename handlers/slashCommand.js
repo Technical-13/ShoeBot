d126de2214d9aca8680fa82155478248d8360b39
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-02 15:18:53 -0400

    Just looking for an emoji that won't throw off table alignment not.

--- handlers/slashCommand.js
+++ handlers/slashCommand.js
@@ -40,7 +40,7 @@ module.exports = ( client ) => {
               } );
               client.slashCommands.set( slashCommand.name, slashCommand );
               arrCmdRow[ 1 ] = cmdName;
-              arrCmdRow.push( '🟨' );
+              arrCmdRow.push( '☑️' );
             } else if ( slashCommand.name ) {
               slashCommands.push( {
                   name: slashCommand.name,
