3edf5ddef3829e8427345b749065a20abcab8df5
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-02 09:01:32 -0400

    Modal didn't work
    modals only work on /commands and not message commands I guess

--- commands/admin/presence.js
+++ commands/admin/presence.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ModalBuilder, StringSelectMenuBuilder, TextInputBuilder, TextInputStyle } = require( 'discord.js' );
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, EmbedBuilder } = require( 'discord.js' );
 
 module.exports = {
     name: 'presence',
@@ -12,43 +12,38 @@ module.exports = {
         message.delete();
 
         const activityTypes = { 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5 };
+        const embedPresence = new EmbedBuilder()
+        .setTitle( 'setPresence' )
+        .setDescription( 'Set my activity, description, and status.' )
+        .setColor( '#FF00FF' )
+        .setTimestamp()
+        .setThumbnail( bot.displayAvatarURL() )
+        .setFooter( { text: bot.tag } )
 
-        const modalPresence = new ModalBuilder( {
-            customId: 'setPresence-' + bot.id,
-            title: 'setPresence'
+        const statusButtons = new ActionRowBuilder().addComponents(
+            new ButtonBuilder().setLabel( 'Offline' ).setCustomId( 'offline' ).setStyle( ButtonStyle.Secondary ),
+            new ButtonBuilder().setLabel( 'Do Not Disturb' ).setCustomId( 'dnd' ).setStyle( ButtonStyle.Danger ),
+            new ButtonBuilder().setLabel( 'Idle' ).setCustomId( 'primary' ).setStyle( ButtonStyle.Link ).setURL( 'https://discord.me/MagentaRV' ),
+            new ButtonBuilder().setLabel( 'Online' ).setCustomId( 'online' ).setStyle( ButtonStyle.Premium ).setSKUId( '1290347242149904405' ) );
+        const embedInterface = await message.reply( { embeds: [ embedPresence ], components: [ statusButtons ] } );
+        const clickFilter = ( clicker ) => { clicker.user.id === author.id };
+        const buttonClicks = embedInterface.createMessageComponentCollector( { componentType: ComponentType.Button, clickFilter, maxProcessed: 1 } );
+
+        buttonClicks.on( 'collect', interaction => {
+            switch ( interaction.customId ) {
+                case 'offline':
+                    bot.setPresence( { status: 'offline' } );break;
+                case 'dnd':
+                    bot.setPresence( { status: 'dnd' } );break;
+                case 'idle':
+                    bot.setPresence( { status: 'idle' } );break;
+                case 'online': default:
+                    bot.setPresence( { status: 'online' } );
+            }
         } );
 
-        const getActivityType = new StringSelectMenuBuilder( {
-            customId: 'activityType',
-            maxValue: 1,
-            options: [ 'playing', 'streaming', 'listening', 'watching', 'custom', 'competing' ]
-        } );
-        const rowActivityType = new ActionRowBuilder().addComponents( getActivityType );
-
-        const getActivityText = new TextInputBuilder( {
-            customId: 'customText',
-            label: 'Custom Text',
-            style: TextInputStyle.Short
-        } );
-        const rowActivityText = new ActionRowBuilder().addComponents( getActivityText );
-
-        const getStatus = new StringSelectMenuBuilder( {
-            customId: 'status',
-            maxValue: 1,
-            options: [ 'offline', 'dnd', 'idle', 'online' ],
-            placeholder: bot.presence.status
-        } );
-        const rowStatus = new ActionRowBuilder().addComponents( getStatus );
-
-        modalPresence.addComponents( rowActivityType, rowActivityText, rowStatus );
-
-        await message.showModal( modalPresence );
-        const inputFilter = ( inputer ) => { inputer.user.id === author.id };
-        message.awaitModalSubmit( { inputFilter, time: 300000 } ).then( modalInteraction => {
-            const activityTypeSelect = modalInteraction.fields.getStringSelectMenuValue( 'activityType' );
-            const activityTextInput = modalInteraction.fields.getTextInputValue( 'customText' );
-            const statusSelect = modalInteraction.fields.getStringSelectMenuValue( 'status' );
-            bot.setPresence( { activities: [ { type: activityTypes[ activityTypeSelect ], name: activityTextInput } ], status: statusSelect } );
+        buttonClicks.on( 'end', () => {
+            setTimeout( () => { embedInterface.delete(); }, 3000 );
         } );
     }
 };
