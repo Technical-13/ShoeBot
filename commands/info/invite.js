35d1a9fc824128679663d80145338f496b570ecf
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-03 14:03:15 -0400

    Use client.generateInvite()

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require( 'discord.js' );
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, OAuth2Scopes, PermissionFlagsBits } = require( 'discord.js' );
 
 module.exports = {
 	name: 'invite',
@@ -7,7 +7,26 @@ module.exports = {
 	userPerms: [ 'Administrator' ],
 	botPerms: [ 'Administrator' ],
 	run: async ( client, message, args ) => {
-		const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=8&scope=bot%20applications.commands`;
+//		const inviteUrl = `https://discord.com/api/oauth2/authorize?client_id=${process.env.CLIENT_ID}&permissions=8&scope=bot%20applications.commands`;
+		const inviteUrl = client.generateInvite( {
+      permissions: [
+        PermissionFlagsBits.CreateInstantInvite,
+        PermissionFlagsBits.Administrator,
+        PermissionFlagsBits.AddReactions,
+        PermissionFlagsBits.ViewChannel,
+        PermissionFlagsBits.SendMessages,
+        PermissionFlagsBits.EmbedLinks,
+        PermissionFlagsBits.AttachFiles,
+        PermissionFlagsBits.ReadMessageHistory,
+        PermissionFlagsBits.UseExternalEmojis,
+        PermissionFlagsBits.ManageWebhooks,
+        PermissionFlagsBits.UseApplicationCommands
+      ],
+      scopes: [
+        OAuth2Scopes.Bot,
+        OAuth2Scopes.ApplicationsCommands
+      ],
+    } );
 		const embed = new EmbedBuilder()
 		.setTitle( 'Invite me' )
 		.setDescription( `Invite the bot to your server. [Click here](${inviteUrl})` )
