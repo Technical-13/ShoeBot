ad762bd9bcdb2ec25ba11f7b480f8019e560b8c6
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-04 16:41:34 -0400

    Update pagination.js

--- functions/pagination.js
+++ functions/pagination.js
@@ -48,7 +48,8 @@ module.exports = async ( interaction, pages, intPageNumber = 0, time = 30 ) => {
       collector.resetTimer();
     } );
 
-    collector.on( 'end', async () => { return await msg.delete(); } );
+    collector.on( 'end', async () => { await msg.delete(); } );
 
+    return msg;
   } catch ( errPagination ) { console.error( 'Error in pagination.js: %s\n%o', errPagination.message, errPagination.stack ); }
 };
