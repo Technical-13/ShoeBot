e71b0e81cd63183860fea64c37d0cadbadd477c2
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-19 16:07:28 -0400

    Carry less likely to crash commands from Signal

--- slashCommands/fun/lmgtfy.js
+++ slashCommands/fun/lmgtfy.js
@@ -1,35 +1,44 @@
 const { ApplicationCommandType } = require( 'discord.js' );
+const userPerms = require( '../../functions/getPerms.js' );
 
 module.exports = {
-    name: 'lmgtfy',
-    description: 'Let Me Google That For You!',
-    type: ApplicationCommandType.ChatInput,
-    options: [ {
-        name: 'query',
-        description: 'What do you want me to look up?',
-        required: true,
-        type: 3
-    }, {
-        name: 'target',
-        description: 'Who should I mention with my response? (Default: yourself)',
-        type: 6
-    }, {
-        name: 'nice',
-        description: 'Should I be nice?',
-        type: 5
-    } ],
-    cooldown: 1000,
-    run: async ( client, interaction ) => {
-        const botOwner = client.users.cache.get( process.env.OWNER_ID );
-        const { channel, guild, options } = interaction;
-        const author = interaction.user;
-        const cmdInputUser = options.getUser( 'target' );
-        const mentionUser = '<@' + ( cmdInputUser ? cmdInputUser.id : author.id ) + '>: ';
-        const beNice = ( options.getBoolean( 'nice' ) || ( cmdInputUser === author ? true : false ) );
-        const service = ( beNice ? 'www.google.com/search' : 'letmegooglethat.com/' );
-        const strInputQuery = options.getString( 'query' );
-        const q = encodeURI( strInputQuery.replace( / /g, '+' ) );
+  name: 'lmgtfy',
+  description: 'Let Me Google That For You!',
+  type: ApplicationCommandType.ChatInput,
+  options: [ {
+    name: 'query',
+    description: 'What do you want me to look up?',
+    required: true,
+    type: 3
+  }, {
+    name: 'target',
+    description: 'Who should I mention with my response? (Default: yourself)',
+    type: 6
+  }, {
+    name: 'nice',
+    description: 'Should I be nice?',
+    type: 5
+  } ],
+  cooldown: 1000,
+  run: async ( client, interaction ) => {
+    const { channel, guild, options } = interaction;
+    const author = interaction.user;
+    const { botOwner, isBotMod, isBlacklisted, isGlobalWhitelisted, guildOwner, isGuildBlacklisted } = await userPerms( client, author, guild );
+    if ( isBlacklisted && !isGlobalWhitelisted ) {
+      let contact = ( isGuildBlacklisted ? guildOwner.id : botOwner.id );
+      return interaction.editReply( { content: 'Oh no!  It looks like you have been blacklisted from using my commands' + ( isGuildBlacklisted ? ' in this server!' : '!' ) + '  Please contact <@' + contact + '> to resolve the situation.' } );
+    }
+    else if ( isBotMod && isGuildBlacklisted ) {
+      author.send( 'You have been blacklisted from using commands in https://discord.com/channels/' + guild.id + '/' + channel.id + '! Use `/config remove` to remove yourself from the blacklist.' );
+    }
 
-        interaction.reply( { content: mentionUser + '<https://' + service + '?q=' + q + '>' } );
-	}
+    const cmdInputUser = options.getUser( 'target' );
+    const mentionUser = '<@' + ( cmdInputUser ? cmdInputUser.id : author.id ) + '>: ';
+    const beNice = ( options.getBoolean( 'nice' ) || ( cmdInputUser === author ? true : false ) );
+    const service = ( beNice ? 'www.google.com/search' : 'letmegooglethat.com/' );
+    const strInputQuery = options.getString( 'query' );
+    const q = encodeURI( strInputQuery.replace( / /g, '+' ) );
+
+    interaction.reply( { content: mentionUser + '<https://' + service + '?q=' + q + '>' } );
+  }
 };
--- slashCommands/info/guilds.js
+++ slashCommands/info/guilds.js
@@ -1,3 +1,6 @@
+const botConfigDB = require( '../../models/BotConfig.js' );
+const guildConfigDB = require( '../../models/GuildConfig.js' );
+const { model, Schema } = require( 'mongoose' );
 const { ApplicationCommandType, EmbedBuilder } = require( 'discord.js' );
 const pagination = require( '../../functions/pagination.js' );
 
@@ -9,7 +12,13 @@ module.exports = {
     { type: 3, name: 'guild', description: 'Start with a specific guild by ID (invalid input will be ignored). (default current or first guild)' }
   ],
   cooldown: 300000,
+  modCmd: true,
   run: async ( client, interaction ) => {
+    await interaction.deferReply();
+
+    const guildConfigs = await guildConfigDB.find();
+    const guildConfigIds = [];
+    guildConfigs.forEach( ( entry, i ) => { guildConfigIds.push( entry.Guild ); } );
 
     const bot = client.user;
     const { channel, guild, options } = interaction;
@@ -17,9 +26,7 @@ module.exports = {
     const botOwner = client.users.cache.get( process.env.OWNER_ID );
     const isBotOwner = ( author.id === botOwner.id ? true : false );
     const botMods = [];
-    const isBotMod = ( ( botOwner || botMods.indexOf( author.id ) != -1 ) ? true : false );
-
-    if ( !botOwner ) { interaction.reply( { content: 'You are not the boss of me...', ephemeral: true } ); return; }
+    const isBotMod = ( ( isBotOwner || botMods.indexOf( author.id ) != -1 ) ? true : false );
 
     const embedGuilds = [], myInvites = [];
     const guildIds = Array.from( client.guilds.cache.keys() );
@@ -33,8 +40,26 @@ module.exports = {
       const guild = client.guilds.cache.get( guildId );
       const objGuild = guild.toJSON();
       const guildName = objGuild.name;
+      const vanityURLCode = objGuild.vanityURLCode;
+if ( vanityURLCode ) { console.log( '%s has a vanityURLCode: %s', guildName, vanityURLCode ); }//don't know what this looks like in the API...
+      const chanWidget = ( objGuild.widgetEnabled ? objGuild.widgetChannelId : null );
+      const chanRules = objGuild.rulesChannelId;
+      const chanPublicUpdates = objGuild.publicUpdatesChannelId;
+      const chanSafetyAlerts = objGuild.safetyAlertsChannelId;
+      const chanSystem = objGuild.systemChannelId;
+      const chanFirst = guild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
+      const doneConfig = ( guildConfigIds.indexOf( guildId ) != -1 ? true : false );
+      const definedInvite = ( doneConfig ? guildConfigs[ guildConfigIds.indexOf( guildId ) ].Invite : null );
+      const chanInvite = ( definedInvite || chanWidget || chanRules || chanPublicUpdates || chanSafetyAlerts || chanSystem || chanFirst );
+      const chanLinkUrl = 'https://discordapp.com/channels/' + guildId + '/' + chanInvite;
       const ownerId = objGuild.ownerId;
       const objGuildOwner = guild.members.cache.get( ownerId );
+      if ( !objGuildOwner ) {
+        await guild.leave()
+          .then( left => { console.log( 'I left guild (%s) with no owner!\n\t%s', left.name, chanLinkUrl ); } )
+          .catch( stayed => { console.error( 'I could NOT leave guild with no owner!\n%o', stayed ); } );
+        continue;
+      }
       const ownerName = objGuildOwner.displayName;
       const iconURL = objGuild.iconURL;
       const memberCount = objGuild.memberCount;
@@ -48,16 +73,6 @@ module.exports = {
       const arrVerificationLevels = [ 'None', 'Low (email)', 'Medium (5m on Discord)', 'High (10m in guild)', 'Very High (phone number)' ];
       const verificationLevel = arrVerificationLevels[ ( objGuild.verificationLevel || 0 ) ];
       const mfaLevel = objGuild.mfaLevel;
-      const vanityURLCode = objGuild.vanityURLCode;
-      if ( vanityURLCode ) { console.log( '%s has a vanityURLCode: %s', guildName, vanityURLCode ); }
-      const chanWidget = ( objGuild.widgetEnabled ? objGuild.widgetChannelId : null );
-      const chanRules = objGuild.rulesChannelId;
-      const chanPublicUpdates = objGuild.publicUpdatesChannelId;
-      const chanSafetyAlerts = objGuild.safetyAlertsChannelId;
-      const chanSystem = objGuild.systemChannelId;
-      const chanFirst = guild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
-      const chanInvite = ( chanWidget || chanRules || chanPublicUpdates || chanSafetyAlerts || chanSystem || chanFirst );
-      const chanLinkUrl = 'https://discordapp.com/channels/' + guildId + '/' + chanInvite;
       const guildInvite = await guild.invites.create( chanInvite, {
         maxAge: 900,
         reason: 'Invite created by ' + author.displayName + ' with `/guilds`.'
@@ -66,6 +81,9 @@ module.exports = {
         return 'https://discord.gg/invite/' + invite.code;
       } ).catch( errCreateInvite => {
         switch ( errCreateInvite.code ) {
+          case 10003://Unknown Channel
+            console.log( 'Unknown channel to create invite for %s:\n\tLink: %s', guildName, chanLinkUrl );
+            break;
           case 50013://Missing permissions
             objGuildOwner.send( 'Help!  Please give me `CreateInstantInvite` permission in ' + chanLinkUrl + '!' ).catch( errSendGuildOwner => {
               console.error( 'Unable to DM guild owner, %s, for %s to get `CreateInstantInvite` permission:\n%o', objGuildOwner.displayName, guildName, errSendGuildOwner );
