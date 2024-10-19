89f6c3f1747bdea5f8decdd20458a9e7612af043
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-19 16:08:54 -0400

    Next easiest to fix batch - /geocaching

--- slashCommands/geocaching/badgebar.js
+++ slashCommands/geocaching/badgebar.js
@@ -1,83 +1,96 @@
-const logSchema = require( '../../models/GuildConfig.js' );
-const { model, Schema } = require( 'mongoose' );
-const { ApplicationCommandType } = require( 'discord.js' );
+const { ApplicationCommandType, InteractionContextType } = require( 'discord.js' );
+const userPerms = require( '../../functions/getPerms.js' );
+const logChans = require( '../../functions/getLogChans.js' );
+const errHandler = require( '../../functions/errorHandler.js' );
 
 module.exports = {
-    name: 'badgebar',
-    description: 'Show Project-GC BadgeBar for user.',
+  name: 'badgebar',
+  description: 'Show Project-GC BadgeBar for user.',
+  description_localizations: {
+    de: 'Project-GC BadgeBar für Benutzer anzeigen.',
+    fi: 'Näytä Project-GC BadgeBar käyttäjälle.',
+    pl: 'Pokaż pasek odznak Project-GC dla użytkownika.' },
+  options: [ {
+    name: 'gc-name',
+    name_localizations: {
+      de: 'gc-name',
+      fr: 'nom-gc',
+      fi: 'gc-nimi',
+      pl: 'gc-name',
+      'sv-SE': 'gc-namn' },
+    description: 'The case-sensitive Geocaching.com username.',
     description_localizations: {
-        de: 'Project-GC BadgeBar für Benutzer anzeigen.',
-        fi: 'Näytä Project-GC BadgeBar käyttäjälle.',
-        pl: 'Pokaż pasek odznak Project-GC dla użytkownika.' },
-    options: [ {
-        name: 'gc-name',
-        name_localizations: {
-            de: 'gc-name',
-            fr: 'nom-gc',
-            fi: 'gc-nimi',
-            pl: 'gc-name',
-            'sv-SE': 'gc-namn' },
-        description: 'The case-sensitive Geocaching.com username.',
-        description_localizations: {
-            de: 'Der Geocaching.com-Benutzername, bei dem die Groß-/Kleinschreibung beachtet werden muss.',
-            fi: 'Geocaching.com-käyttäjänimi, kirjainkoko merkitsevä.',
-            pl: 'W nazwie użytkownika Geocaching.com rozróżniana jest wielkość liter.' },
-        type: 3
-    }, {
-        name: 'discord-user',
-        name_localizations: {
-            de: 'discord-benutzer',
-            fr: 'utilisateur-discord',
-            fi: 'discord-käyttäjä',
-            pl: 'discord-użytkownik',
-            'sv-SE': 'discord-användare' },
-        description: 'Discord member (requires nickname to be set if different from GC name).',
-        description_localizations: {
-            de: 'Discord-Mitglied (erfordert das Festlegen eines Spitznamens, wenn dieser vom GC-Namen abweicht).',
-            fi: 'Discord-jäsen (vaatii nimimerkin asettamisen, jos se on eri kuin GC-nimi).',
-            pl: 'Członek Discord (wymaga ustawienia pseudonimu, jeśli różni się od nazwy GC).' },
-        type: 6
-    } ],
-    type: ApplicationCommandType.ChatInput,
-    cooldown: 1000,
-    run: async ( client, interaction ) => {
-        await interaction.deferReply();
-        const { channel, guild, options } = interaction;
-        const author = interaction.user;
-        const botOwner = client.users.cache.get( process.env.OWNER_ID );
-        const isBotOwner = ( author.id === botOwner.id ? true : false );
-        const botMods = [];
-        const isBotMod = ( ( botOwner || botMods.indexOf( author.id ) != -1 ) ? true : false );
-        const objGuildMembers = guild.members.cache;
-        const objGuildOwner = objGuildMembers.get( guild.ownerId );
-        const isGuildOwner = ( author.id === objGuildOwner.id ? true : false );
-        const arrAuthorPermissions = ( guild.members.cache.get( author.id ).permissions.toArray() || [] );
-        var logChan = objGuildOwner;
-        var logErrorChan = objGuildOwner;      
-
-        const today = ( new Date() );
-        const intYear = today.getFullYear();
-        const intMonthNow = today.getMonth();
-        const intMonth = ( intMonthNow < 9 ? '0' + ( intMonthNow + 1 ).toString() : ( intMonthNow + 1 ).toString() );
-        const intDayNow = today.getDate();
-        const intDay = ( intDayNow <= 9 ? '0' + intDayNow.toString() : intDayNow.toString() );
-
-        const strAuthorDisplayName = objGuildMembers.get( author.id ).displayName;
-        const strInputUser = ( options.getString( 'gc-name' ) || null );
-        const objInputUser = ( options.getUser( 'discord-user' ) || null );
-        const strInputUserDisplayName = ( objInputUser ? objInputUser.displayName : strInputUser );
-        const strUseName = ( strInputUserDisplayName ? strInputUserDisplayName : strAuthorDisplayName );
-        const encName = encodeURI( strUseName ).replace( '&', '%26' );
-
-
-        logSchema.findOne( { Guild: guild.id } ).then( async data => {
-            if ( data ) {
-                if ( data.Logs.Chat ) { logChan = await guild.channels.cache.get( data.Logs.Default ); }
-                if ( data.Logs.Error ) { logErrorChan = guild.channels.cache.get( data.Logs.Error ); }
-            }
-            let setupPlease = ( logChan == objGuildOwner ? 'Please run `/config` to have these logs go to a channel in the server instead of your DMs.' : '----' );
-
-            interaction.editReply( { content: 'BadgeBar for: ' + ( objInputUser == null ? strUseName : '<@' +  objInputUser + '>' ) + '\nhttps://cdn2.project-gc.com/BadgeBar/' + encName + '.png#' + intYear + '-' + intMonth + '-' + intDay } );
-        } );
+      de: 'Der Geocaching.com-Benutzername, bei dem die Groß-/Kleinschreibung beachtet werden muss.',
+      fi: 'Geocaching.com-käyttäjänimi, kirjainkoko merkitsevä.',
+      pl: 'W nazwie użytkownika Geocaching.com rozróżniana jest wielkość liter.' },
+    type: 3
+  }, {
+    name: 'discord-user',
+    name_localizations: {
+      de: 'discord-benutzer',
+      fr: 'utilisateur-discord',
+      fi: 'discord-käyttäjä',
+      pl: 'discord-użytkownik',
+      'sv-SE': 'discord-användare' },
+    description: 'Discord member (requires nickname to be set if different from GC name).',
+    description_localizations: {
+      de: 'Discord-Mitglied (erfordert das Festlegen eines Spitznamens, wenn dieser vom GC-Namen abweicht).',
+      fi: 'Discord-jäsen (vaatii nimimerkin asettamisen, jos se on eri kuin GC-nimi).',
+      pl: 'Członek Discord (wymaga ustawienia pseudonimu, jeśli różni się od nazwy GC).' },
+    type: 6
+  } ],
+  type: ApplicationCommandType.ChatInput,
+  contexts: [ InteractionContextType.Guild ],
+  cooldown: 1000,
+  run: async ( client, interaction ) => {
+    await interaction.deferReply( { ephemeral: true } );
+    const { channel, guild, options, user: author } = interaction;
+    const { members.cache: members } = guild;
+    const { botOwner, isBotMod, isBlacklisted, isGlobalWhitelisted, guildOwner, isGuildBlacklisted } = await userPerms( author, guild );
+    if ( isBlacklisted && !isGlobalWhitelisted ) {
+      let contact = ( isGuildBlacklisted ? guildOwner.id : botOwner.id );
+      return interaction.editReply( { content: 'Oh no!  It looks like you have been blacklisted from using my commands' + ( isGuildBlacklisted ? ' in this server!' : '!' ) + '  Please contact <@' + contact + '> to resolve the situation.' } );
     }
+    else if ( isBotMod && isGuildBlacklisted ) { author.send( { content:
+      'You have been blacklisted from using commands in https://discord.com/channels/' + guild.id + '! Use `/config remove` to remove yourself from the blacklist.'
+    } ); }
+
+    const today = ( new Date() );
+    const intYear = today.getFullYear();
+    const intMonthNow = today.getMonth();
+    const intMonth = ( intMonthNow < 9 ? '0' + ( intMonthNow + 1 ).toString() : ( intMonthNow + 1 ).toString() );
+    const intDayNow = today.getDate();
+    const intDay = ( intDayNow <= 9 ? '0' + intDayNow.toString() : intDayNow.toString() );
+
+    const strAuthorDisplayName = members.get( author.id ).displayName;
+    const strInputUser = ( options.getString( 'gc-name' ) || null );
+    const objInputUser = ( options.getUser( 'discord-user' ) || null );
+    const strInputUserDisplayName = ( objInputUser ? objInputUser.displayName : strInputUser );
+    const strUseName = ( strInputUserDisplayName ? strInputUserDisplayName : strAuthorDisplayName );
+    const encName = encodeURI( strUseName ).replace( '&', '%26' );
+
+    const { doLogs, chanDefault, chanError, strClosing } = await logChans( guild );
+
+    channel.send( { content:
+      'BadgeBar for ' + ( objInputUser ? '<@' +  objInputUser.id + '>' : strUseName ) +
+      ( strInputUserDisplayName !== strAuthorDisplayName ? ' as requested by <@' + author.id + '>' : '' ) +
+      ':\nhttps://cdn2.project-gc.com/BadgeBar/' + encName + '.png#' + intYear + '-' + intMonth + '-' + intDay
+    } )
+    .then( sentMsg => {
+      if ( doLogs && strInputUserDisplayName !== strAuthorDisplayName ) {
+        chanDefault.send( { content:
+          'I shared the `/badgebar` for ' + ( objInputUser ? '<@' +  objInputUser.id + '>' : strUseName ) + ' in <#' + channel.id + '>' +
+          ( strInputUserDisplayName !== strAuthorDisplayName ? ' as requested by <@' + author.id + '>' : '' ) + strClosing } )
+        .catch( errLog => { await errHandler( errLog, { chanType: 'default', command: 'badgebar', guild: guild, type: 'logLogs' } ); } );
+      }
+      interaction.deleteReply();
+    } )
+    .catch( errSend => {
+      console.error( 'Error sending /badgebar result to %s#%s:\n%o', guild.name, channel.name, errSend );
+      if ( doLogs ) {
+        chanError.send( { content: 'Error sending `/badgebar` result to <#' + channel.id + '>' + strClosing } )
+        .catch( errLog => { await errHandler( errLog, { chanType: 'error', command: 'badgebar', guild: guild, type: 'logLogs' } ); } );
+      }
+    } );    
+  }
 };
---
+++ slashCommands/geocaching/cipher.js
@@ -0,0 +1,29 @@
+const { ApplicationCommandType, InteractionContextType } = require( 'discord.js' );
+const userPerms = require( '../../functions/getPerms.js' );
+
+module.exports = {
+  name: 'decode',
+  description: 'Cipher (de|en)coder.',
+  type: ApplicationCommandType.ChatInput,
+  options: [
+    { type: 3, name: 'string', description: 'string to (de|en)code.', required: true },
+    { type: 3, name: 'use-type', description: '' },
+    { type: 10, name: 'numeric', description: 'Characters in the Latin alphabet. (default 5)', minValue: 1, maxValue: 10 },
+    { type: 10, name: 'alphabetic', description: 'Characters in the Latin alphabet. (default 13)', minValue: 1, maxValue: 26 },
+    { type: 10, name: 'alphanumberic', description: 'Characters in the Latin alphabet. (default 18)', minValue: 1, maxValue: 36 }
+  modOnly: true,
+  cooldown: 1000,
+  run: async ( client, interaction ) => {
+    await interaction.deferReply( { ephemeral: true } );
+    const { channel, guild, options, user: author } = interaction;
+    const { botOwner, isBotMod, isBlacklisted, isGlobalWhitelisted, guildOwner, isGuildBlacklisted } = await userPerms( client, author, guild );
+    if ( isBlacklisted && !isGlobalWhitelisted ) {
+      let contact = ( isGuildBlacklisted ? guildOwner.id : botOwner.id );
+      return interaction.editReply( { content: 'Oh no!  It looks like you have been blacklisted from using my commands' + ( isGuildBlacklisted ? ' in this server!' : '!' ) + '  Please contact <@' + contact + '> to resolve the situation.' } );
+    }
+    else if ( isBotMod && isGuildBlacklisted ) {
+      author.send( 'You have been blacklisted from using commands in https://discord.com/channels/' + guild.id + '/' + channel.id + '! Use `/config remove` to remove yourself from the blacklist.' );
+    }
+    return interaction.editReply( { content: 'Comming soon:tm:' } );
+  }
+};
