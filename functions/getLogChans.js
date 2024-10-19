b34f26cd4dcc32403cf57fa61db93157b5273360
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-19 18:52:44 -0400

    Update getLogChans.js

--- functions/getLogChans.js
+++ functions/getLogChans.js
@@ -25,12 +25,12 @@ module.exports = async ( guild ) => {
     }
     else {
       const doLogs = ( guildConfig.Logs.Active || true );
-      const guildOwner = guild.members.get( guild.ownerId );
+      const guildOwner = guild.members.cache.get( guild.ownerId );
       const chanDefault = ( guildConfig.Logs.Default ? guild.channels.cache.get( guildConfig.Logs.Default ) : guildOwner );
       const chanError = ( guildConfig.Logs.Error ? guild.channels.cache.get( guildConfig.Logs.Error ) : chanDefault );
       const chanChat = ( guildConfig.Logs.Chat ? guild.channels.cache.get( guildConfig.Logs.Chat ) : chanDefault );
       
-      const strClosing = '\n' + ( chanDefault == guildOwner ? 'Please run `/config` to have these logs go to a channel in the [' + guild.name + '](<https://discord.com/channels/' + guild.id + '>) server or deactivate them instead of your DMs.' : '----' );
+      const strClosing = '\n' + ( chanDefault == guildOwner ? 'Please run `/config set` to have these logs go to a channel in the [' + guild.name + '](<https://discord.com/channels/' + guild.id + '>) server or deactivate them instead of to your DMs.' : '----' );
     
       return {
         doLogs: doLogs,
