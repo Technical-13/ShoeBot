b1f3ada4984cde81d048d98a3e0e0f741f79b5b6
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-23 14:15:07 -0400

    Update index.js
    No need to await it if we're not getting anything back from it even though it was returning...

--- index.js
+++ index.js
@@ -5,46 +5,43 @@ const { Client, GatewayIntentBits, Partials, Collection } = require( 'discord.js
 const config = require( './config.json' );
 require( 'dotenv' ).config();
 
-( async () => {
-  const botConfig = await initDatabase();
-  console.log( 'Got my configuration from database:\n%o', botConfig );
+initDatabase();
 
-  const client = new Client( {
-    intents: [
-      GatewayIntentBits.Guilds, 
-      GatewayIntentBits.GuildMessages, 
-      GatewayIntentBits.GuildPresences, 
-      GatewayIntentBits.GuildMessageReactions, 
-      GatewayIntentBits.DirectMessages,
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.MessageContent,
-    ], 
-    partials: [ Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction ]
-  } );
+const client = new Client( {
+  intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.GuildPresences, 
+    GatewayIntentBits.GuildMessageReactions, 
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ], 
+  partials: [ Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction ]
+} );
 
 /* -------------------- COLLECTIONS -------------------- */
-  client.aliases = new Collection();
-  client.commands = new Collection();
-  client.groups = new Collection();
-  client.events = new Collection();
-  client.prefix = config.prefix;
-  client.slashCommands = new Collection();
+client.aliases = new Collection();
+client.commands = new Collection();
+client.groups = new Collection();
+client.events = new Collection();
+client.prefix = config.prefix;
+client.slashCommands = new Collection();
 
 /* ------------------ STATIC COMMANDS ------------------ */
-  var staticCmds = [];
-  if ( config.staticCmds ) { staticCmds.concat( config.staticCmds ); }
-  staticCmds.push( 'admin' );
-  client.groups.set( 'staticCmds', staticCmds );
+var staticCmds = [];
+if ( config.staticCmds ) { staticCmds.concat( config.staticCmds ); }
+staticCmds.push( 'admin' );
+client.groups.set( 'staticCmds', staticCmds );
 
-  module.exports = client;
+module.exports = client;
 
-  fs.readdirSync( './handlers' ).forEach( ( handler ) => {
-    require( `./handlers/${handler}` )( client );
-  } );
+fs.readdirSync( './handlers' ).forEach( ( handler ) => {
+  require( `./handlers/${handler}` )( client );
+} );
 
-  client.login( process.env.token )
-  .then( async loggedIn => { console.log( 'Successfully connected!' ); } )
-  .catch( errLogin => { console.error( 'There was an error logging in:\n%s', errLogin.stack ); } );
-} )();
+client.login( process.env.token )
+.then( async loggedIn => { console.log( 'Successfully connected!' ); } )
+.catch( errLogin => { console.error( 'There was an error logging in:\n%s', errLogin.stack ); } );
 
 keepAlive();
