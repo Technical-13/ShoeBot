e6bab0ec857901fa902870813c0d8abbf2950062
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 11:29:49 -0400

    Cycle through some stats and custom presence activities.

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,11 @@
 const client = require( '..' );
 const chalk = require( 'chalk' );
+const config = require( '../config.json' );
 
 client.on( 'ready', async rdy => {
+  const activityTypes = { 'Playing': 0, 'Streaming': 1, 'Listening': 2, 'Watching': 3, 'Custom': 4, 'Competing': 5 };
+  await client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: '🥱 Just waking up...' } ], status: 'dnd' } );
+
   const today = ( new Date() );
   const objTimeString = {"hour":"2-digit","hourCycle":"h24","minute":"2-digit","second":"2-digit","timeZone":"America/New_York","timeZoneName":"short"};
   const botTime = today.toLocaleTimeString( 'en-US', objTimeString );
@@ -9,11 +13,20 @@ client.on( 'ready', async rdy => {
   const hour = parseInt( botTime.split( ':' )[ 0 ] );
   const myTime = ( hour >= 5 && hour < 12 ? 'morning' : ( hour >= 12 && hour < 18 ? 'evening' : 'nighttime' ) );
   const myCup = ( hour >= 5 && hour < 12 ? 'my ' : ( hour >= 12 && hour < 18 ? 'an ' : 'a ' ) ) + myTime;
-  const activityTypes = { 'Playing': 0, 'Streaming': 1, 'Listening': 2, 'Watching': 3, 'Custom': 4, 'Competing': 5 };
-  client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: '🥱 Just waking up...' } ], status: 'dnd' } );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Watching, name: 'my ' + myTime + ' coffee brew...' } ], status: 'dnd' } ); }, 15000 );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: 'Drinking ' + myCup + ' cup of ☕' } ], status: 'idle' } ); }, 60000 );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: 'https://discord.me/MagentaRV' } ], status: 'online' } ); }, 180000 );
+  setTimeout( async () => { await client.user.setPresence( { activities: [ { type: activityTypes.Watching, name: 'my ' + myTime + ' coffee brew...' } ], status: 'dnd' } ); }, 15000 );
+  setTimeout( async () => { await client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: 'Drinking ' + myCup + ' cup of ☕' } ], status: 'idle' } ); }, 60000 );
+  const firstActivity = config.activities[ 0 ];
+  setTimeout( async () => { await client.user.setPresence( { activities: [ { type: activityTypes[ firstActivity.type ], name: firstActivity.name } ], status: 'online' } ); }, 180000 );
+
+  const servingGuilds = [ { type: 'Listening', name: 'for activity in ' + client.guilds.size + ' servers.' } ];
+  const servingUsers = [ { type: 'Watching', name: 'to the messages of ' + client.users.size + ' members.' } ];
+  const cycleActivities = [].concat( config.activities, servingGuilds, servingUsers );
+  const intActivities = cycleActivities.length;
+  var iAct = 0;
+  setInterval( async () => {
+    let thisActivity = cycleActivities[ iAct++ ];
+    await client.user.setPresence( { activities: [ { type: activityTypes[ thisActivity.type ], name: thisActivity.name } ], status: 'online' } );
+  }, 300000 );
 
   console.log( chalk.bold.magentaBright( `Successfully logged in as: ${client.user.tag}` ) );
 } );
