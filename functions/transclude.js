25d83889f5c1c058a670838c87b4cd3ed779a158
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 17:24:21 -0400

    Have to assign to new string.

--- functions/transclude.js
+++ functions/transclude.js
@@ -39,14 +39,15 @@ module.exports = async ( rawString, obj = { guild: null, member: null } ) => {
   }
 
   arrTemplates = rawString.match( /\{\{((?:bot|guild)\.[a-z\.]*)\}\}/g );
+  var parsed = rawString;
   arrTemplates.forEach( template => {
-    if ( transclusions[ template ] ) { rawString.replace( template, transclusions[ template ] );console.log( 'template transclusions: %o', template ); }
-    else if ( notAvailable[ template ] ) { rawString.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' );console.log( 'template notAvailable: %o', template ); }
+    if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] );console.log( 'template transclusions: %o', template ); }
+    else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' );console.log( 'template notAvailable: %o', template ); }
     else {
-      rawString.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
+      parsed = parsed.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
       console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
     }
   } );
   
-  return rawString;
+  return parsed;
 };
