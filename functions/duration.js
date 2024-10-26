87013efec204d6519cfcc2c9f576bbd6f54c7c3d
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 16:37:40 -0400

    default units

--- functions/duration.js
+++ functions/duration.js
@@ -1,13 +1,13 @@
 const client = require( '..' );
 
-module.exports = async ( ms, getUnits ) => {// getUnits = { getWeeks: false, getDays: true, getHours: true, getMinutes: true, getSeconds: false }
+module.exports = async ( ms, getUnits = { getWeeks: false, getDays: true, getHours: true, getMinutes: true, getSeconds: false } ) => {
   if ( isNaN( ms ) ) { return '∅ms'; }
   const objUnits = {
     wks: ( !getUnits.getWeeks ? false : ( typeof getUnits.getWeeks != 'boolean' ? false : getUnits.getWeeks ) ),
     days: ( !getUnits.getDays ? true : ( typeof getUnits.getDays != 'boolean' ? true : getUnits.getDays ) ),
     hrs: ( !getUnits.getHours ? true : ( typeof getUnits.getHours != 'boolean' ? true : getUnits.getHours ) ),
     min: ( !getUnits.getMinutes ? true : ( typeof getUnits.getMinutes != 'boolean' ? true : getUnits.getMinutes ) ),
-    secs: ( !getUnits.getSeconds ? false : ( typeof getUnits.getSeconds != 'boolean' ? false : getUnits.getSeconds ) ),
+    secs: ( typeof getUnits.getSeconds != 'boolean' ? false : getUnits.getSeconds ),
   };
 
   if ( objUnits.wks || objUnits.days || objUnits.hrs || objUnits.min || objUnits.secs ) {
