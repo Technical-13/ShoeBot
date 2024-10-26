5dca0e0604210c777f19568696d177dbcdaa188b
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 10:47:24 -0400

    Start adding event for guild* and guildMember*

--- events/ready.js
+++ events/ready.js
@@ -9,11 +9,11 @@ client.on( 'ready', async rdy => {
   const hour = parseInt( botTime.split( ':' )[ 0 ] );
   const myTime = ( hour >= 5 && hour < 12 ? 'morning' : ( hour >= 12 && hour < 18 ? 'evening' : 'nighttime' ) );
   const myCup = ( hour >= 5 && hour < 12 ? 'my ' : ( hour >= 12 && hour < 18 ? 'an ' : 'a ' ) ) + myTime;
-  const activityTypes = { 'playing': 0, 'streaming': 1, 'listening': 2, 'watching': 3, 'custom': 4, 'competing': 5 };
-  client.user.setPresence( { activities: [ { type: activityTypes.custom, name: '🥱 Just waking up...' } ], status: 'dnd' } );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.watching, name: 'my ' + myTime + ' coffee brew...' } ], status: 'dnd' } ); }, 15000 );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.custom, name: 'Drinking ' + myCup + ' cup of ☕' } ], status: 'idle' } ); }, 60000 );
-  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.custom, name: 'https://discord.me/MagentaRV' } ], status: 'online' } ); }, 180000 );
+  const activityTypes = { 'Playing': 0, 'Streaming': 1, 'Listening': 2, 'Watching': 3, 'Custom': 4, 'Competing': 5 };
+  client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: '🥱 Just waking up...' } ], status: 'dnd' } );
+  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Watching, name: 'my ' + myTime + ' coffee brew...' } ], status: 'dnd' } ); }, 15000 );
+  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: 'Drinking ' + myCup + ' cup of ☕' } ], status: 'idle' } ); }, 60000 );
+  setTimeout( () => { client.user.setPresence( { activities: [ { type: activityTypes.Custom, name: 'https://discord.me/MagentaRV' } ], status: 'online' } ); }, 180000 );
 
   console.log( chalk.bold.magentaBright( `Successfully logged in as: ${client.user.tag}` ) );
 } );
