eb74e66c54d3e188fcc083f47504eeaefd21aa92
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 17:32:54 -0400

    /* DEBUGGING */

--- functions/parser.js
+++ functions/parser.js
@@ -8,13 +8,14 @@ module.exports = async ( rawString, obj = { guild: null, member: null } ) => {
   const guild = ( member ? member.guild : ( obj.guild ? obj.guild : null ) );
   const bot = client.user;
 
+  const currUptime = await duration( client.uptime );
   const transclusions = {
     '{{bot.name}}': bot.displayName,
     '{{bot.owner.name}}': client.users.cache.get( client.ownerId ).displayName,
     '{{bot.owner.ping}}': '<@' + client.ownerId + '>',
     '{{bot.servers}}': client.guilds.cache.size,
     '{{bot.users}}': client.users.cache.size,
-    '{{bot.uptime}}': await duration( client.uptime )
+    '{{bot.uptime}}': currUptime
   };
   const notAvailable = {};
 
@@ -40,9 +41,15 @@ module.exports = async ( rawString, obj = { guild: null, member: null } ) => {
 
   arrTemplates = rawString.match( /\{\{((?:bot|guild)\.[a-z\.]*)\}\}/g );
   var parsed = rawString;
+/* DEBUGGING */
+console.log( 'currUptime: %o', currUptime );
+console.log( 'transclusions: %o', transclusions );
+console.log( 'notAvailable: %o', notAvailable );
+console.log( 'arrTemplates: %o', arrTemplates );
+/* DEBUGGING */
   arrTemplates.forEach( template => {
-    if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] );console.log( 'template transclusions: %o', template ); }
-    else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' );console.log( 'template notAvailable: %o', template ); }
+    if ( transclusions[ template ] ) { parsed = parsed.replace( template, transclusions[ template ] );/* DEBUGGING */console.log( 'template transclusions: %o', template );/* DEBUGGING */ }
+    else if ( notAvailable[ template ] ) { parsed = parsed.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' );/* DEBUGGING */console.log( 'template notAvailable: %o', template );/* DEBUGGING */ }
     else {
       parsed = parsed.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
       console.log( 'Someone tried to transclude %s, search for a GitHub feature request:\n https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
