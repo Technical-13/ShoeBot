8b93ffb2f2310768bba80dd05b1037f7f6e05a98
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-26 17:12:46 -0400

    Update transclude.js

--- functions/transclude.js
+++ functions/transclude.js
@@ -3,7 +3,9 @@ const config = require( '../config.json' );
 const chalk = require( 'chalk' );
 const duration = require( './duration.js' );
 
-module.exports = async ( rawString, objects = { guild: null, member: null } ) => {
+module.exports = async ( rawString, obj = { guild: null, member: null } ) => {
+  const member = ( obj.member ? obj.member : null );
+  const guild = ( member ? member.guild : ( obj.guild ? obj.guild : null ) );
   const bot = client.user;
 
   const transclusions = {
@@ -14,25 +16,35 @@ module.exports = async ( rawString, objects = { guild: null, member: null } ) =>
     '{{bot.users}}': client.users.cache.size,
     '{{bot.uptime}}': await duration( client.uptime )
   };
-  if ( objects.guild ) {
-    const { guild } = objects;
+  const notAvailable = {};
+
+  if ( guild ) {
     transclusions[ '{{guild.owner.name}}' ] = guild.members.cache.get( guild.ownerId ).displayName;
     transclusions[ '{{guild.owner.ping}}' ] = '<@' + guild.ownerId + '>';
     transclusions[ '{{guild.members}}' ] = guild.members.cache.size;
     transclusions[ '{{guild.name}}' ] = guild.name;
+  } else {
+    notAvailable[ '{{guild.owner.name}}' ] = 'guild';
+    notAvailable[ '{{guild.owner.ping}}' ] = 'guild';
+    notAvailable[ '{{guild.members}}' ] = 'guild';
+    notAvailable[ '{{guild.name}}' ] = 'guild';
   }
-  if ( objects.member ) {
-    const { member } = objects;
+
+  if ( member ) {
     transclusions[ '{{member.name}}' ] = member.displayName;
     transclusions[ '{{member.ping}}' ] = '<@' + member.id + '>';
+  } else {
+    notAvailable[ '{{member.name}}' ] = 'member';
+    notAvailable[ '{{member.ping}}' ] = 'member';
   }
 
   arrTemplates = rawString.match( /\{\{((?:bot|guild)\.[a-z\.]*)\}\}/g );
   arrTemplates.forEach( template => {
     if ( transclusions[ template ] ) { rawString.replace( template, transclusions[ template ] ); }
+    else if ( notAvailable[ template ] ) { rawString.replace( template, '*(unknown ' + notAvailable[ template ] + ')*' ); }
     else {
       rawString.replace( template, '[*' + template + '*](<https://github.com/Technical-13/' + bot.username + '/issues/new?labels=enhancement&template=feature_request.md&title=' + encodeURI( 'Please add ' + template + ' to transclude.js' ) + '>)' );
-      console.log( 'Someone tried to transclude %s.  Search for a GitHub feature_request: https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
+      console.log( 'Someone tried to transclude %s, search for a GitHub feature_request:\n https://github.com/Technical-13/%s/issues?q=%s', chalk.bold.red( template ), bot.username, encodeURI( 'Please add ' + template + ' to transclude.js' ) );
     }
   } );
 };
