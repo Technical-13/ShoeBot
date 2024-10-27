e47736dd948178e7dd724eb5fe1f1c3ab524e307
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-27 10:29:17 -0400

    Not dealing with ms for xs/yrs/mos...

--- functions/duration.js
+++ functions/duration.js
@@ -1,10 +1,8 @@
 const client = require( '..' );
 
 module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, getMonths: false, getWeeks: false, getDays: true, getHours: true, getMinutes: true, getSeconds: false } ) => {
-  /* TRON */console.log( 'ms: %o', ms );/* TROFF */
   if ( isNaN( ms ) ) { return '∅ms'; }
 
-  /* TRON */console.log( 'getUnits: %o', getUnits );/* TROFF */
   const objUnits = ( !getUnits ? { xs: false, yrs: false, mos: false, wks: false, days: true, hrs: true, min: true, secs: false } : {
     xs: ( typeof getUnits.getDecades != 'boolean' ? false : getUnits.getDecades ),
     yrs: ( typeof getUnits.getYears != 'boolean' ? false : getUnits.getYears ),
@@ -15,45 +13,36 @@ module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, ge
     min: ( typeof getUnits.getMinutes != 'boolean' ? true : getUnits.getMinutes ),
     secs: ( typeof getUnits.getSeconds != 'boolean' ? false : getUnits.getSeconds )
   } );
-  /* TRON */console.log( 'objUnits: %o', objUnits );/* TROFF */
 
   if ( objUnits.xs || objUnits.yrs || objUnits.mos || objUnits.wks || objUnits.days || objUnits.hrs || objUnits.min || objUnits.secs ) {
     var intDecades, intYears, intMonths, intWeeks, intDays, intHours, intMinutes, intSeconds;
     var totalSeconds = ( ms / 1000 );
     if ( objUnits.xs ) {
-      intDecades = Math.floor( totalSeconds / 315569520000 );
-      totalSeconds %= 315569520000;
+      intDecades = Math.floor( totalSeconds / 315569520 );
+      totalSeconds %= 315569520;
     }
-    /* TRON */console.log( 'intDecades: %o', intDecades );/* TROFF */
     if ( objUnits.yrs ) {
-      intYears = Math.floor( totalSeconds / 31556952000 );
-      totalSeconds %= 31556952000;
+      intYears = Math.floor( totalSeconds / 31556952 );
+      totalSeconds %= 31556952;
     }
-    /* TRON */console.log( 'intYears: %o', intYears );/* TROFF */
     if ( objUnits.mos ) {
-      intMonths = Math.floor( totalSeconds / 2629746000 );
-      totalSeconds %= 2629746000;
+      intMonths = Math.floor( totalSeconds / 2629746 );
+      totalSeconds %= 2629746;
     }
-    /* TRON */console.log( 'intMonths: %o', intMonths );/* TROFF */
     if ( objUnits.wks && ( intDecades + intYears + intMonths ) === 0 ) {
       intWeeks = Math.floor( totalSeconds / 604800 );
       totalSeconds %= 604800;
     }
-    /* TRON */console.log( 'intWeeks: %o', intWeeks );/* TROFF */
     if ( objUnits.days ) {
       intDays = Math.floor( totalSeconds / 86400 );
       totalSeconds %= 86400;
     }
-    /* TRON */console.log( 'intDays: %o', intDays );/* TROFF */
     if ( objUnits.hrs ) {
       intHours = Math.floor( totalSeconds / 3600 );
       totalSeconds %= 3600;
     }
-    /* TRON */console.log( 'intHours: %o', intHours );/* TROFF */
     if ( objUnits.min ) { intMinutes = Math.floor( totalSeconds / 60 ); }
-    /* TRON */console.log( 'intMinutes: %o', intMinutes );/* TROFF */
     if ( objUnits.secs ) { intSeconds = Math.floor( totalSeconds % 60 ); }
-    /* TRON */console.log( 'intSeconds: %o', intSeconds );/* TROFF */
 
     const result = [];
     if ( objUnits.xs && intDecades != 0 ) { result.push( intDecades + ' decade' + ( intDecades === 1 ? '' : 's' ) ); }
@@ -64,7 +53,6 @@ module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, ge
     if ( objUnits.hrs && intHours != 0 ) { result.push( intHours + ' hour' + ( intHours === 1 ? '' : 's' ) ); }
     if ( objUnits.min && intMinutes != 0 ) { result.push( intMinutes + ' minute' + ( intMinutes === 1 ? '' : 's' ) ); }
     if ( objUnits.secs && intSeconds != 0 ) { result.push( intSeconds + ' second' + ( intSeconds === 1 ? '' : 's' ) ); }
-    /* TRON */console.log( 'result: %o', result );/* TROFF */
 
     return ( result.join() ? result.join( ', ' ) : ms + 'ms' );
   }
