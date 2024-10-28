325178978b67711c26ee127da659317afab8dd77
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-27 20:20:03 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -12,14 +12,14 @@ client.on( 'guildMemberAdd', async ( member ) => {
   .catch( async errFind => {
     console.error( 'Error attempting to find %s (ID:%s) in my database in config.js:\n%s', guild.name, guild.id, errFind.stack );
   } );
-  const doLog = ( !guildConfig ? false : ( !guildconfig.Logs ? false : ( guildconfig.Logs.Active || false ) ) );
+  const doLog = ( !guildConfig ? false : ( !guildConfig.Logs ? false : ( guildConfig.Logs.Active || false ) ) );
   const chanDefaultLog = ( !doLog ? null : member.guild.channels.cache.get( guildConfig.Logs.Default ) );
   const chanErrorLog = ( !doLog ? null : member.guild.channels.cache.get( guildConfig.Logs.Error ) );
-  const doWelcome = ( !guildConfig ? false : ( !guildconfig.Welcome ? false : ( guildconfig.Welcome.Active || false ) ) );
+  const doWelcome = ( !guildConfig ? false : ( !guildConfig.Welcome ? false : ( guildConfig.Welcome.Active || false ) ) );
   if ( doWelcome ) {
-    const welcomeChan = ( !guildconfig.Welcome.Channel ? member : member.guild.channels.cache.get( guildconfig.Welcome.Channel ) );
-    const welcomeMsg = parse( guildconfig.Welcome.Msg || 'Welcome {{member.ping}}!\n**{{server.name}}** now has {{server.members}} members!\nPlease reach out to the server owner, {{server.owner.ping}} if you need any help!', { member: member } );
-    const welcomeRole = ( !guildconfig.Welcome.Role ? null : member.guild.roles.cache.get( guildconfig.Welcome.Role ) );
+    const welcomeChan = ( !guildConfig.Welcome.Channel ? member : member.guild.channels.cache.get( guildConfig.Welcome.Channel ) );
+    const welcomeMsg = parse( guildConfig.Welcome.Msg || 'Welcome {{member.ping}}!\n**{{server.name}}** now has {{server.members}} members!\nPlease reach out to the server owner, {{server.owner.ping}} if you need any help!', { member: member } );
+    const welcomeRole = ( !guildConfig.Welcome.Role ? null : member.guild.roles.cache.get( guildConfig.Welcome.Role ) );
     welcomeChan.send( { content: welcomeMsg } )
     .then( welcomeSent => {
       if ( welcomeRole ) {
