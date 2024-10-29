c9f987a1caa93ecc016897946b47712dbd79a66a
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-29 08:26:22 -0400

    Update roadmap.js

--- commands/admin/roadmap.js
+++ commands/admin/roadmap.js
@@ -7,11 +7,15 @@ module.exports = {
   cooldown: 600000,
   run: async ( client, message, args ) => {
     const arrToDo = [
-      'Add logging for geocaching commands directed to another server member.',
-      'Create something for event logging.',
-      'Add `guild*` and `guildMember*` event listeners.',
-      'Finish `/guilds` command as a paginated embed.',
-      'Create function script to get log channels in commands.',
+      'Add `Version` and `Expires` to GuildConfig.js schema.',
+      'Add `Part` to GuildConfig.js to define what bot does when members leave for any reason. Add settings for this to config.js',
+      'Add `Logs` to BotConfig.js schema.',
+      'Check DB entries are current version in ready.js. Create anything missing and update member count as needed. If expired, delete entry.',
+      'Finish guildCreate.js event. Add new guild to DB if not already there. If it is, recreate if expired or update it if not (removing expiration). Tell guild owner how to use bot.',
+      'Populate guildDelete.js event. Log in system log channel guild left and add expiration to DB for guild to be removed from DB after 2 weeks.',
+      'Create guildUpdate.js to update DB entry if guild.name changes.',
+      'Do whatever is defined for guildConfig.Part in guildMemberKick.js, guildMemberBan.js, and/or guildMemberDelete.js',
+      'Create `/verify` command.'.
       'Add some more stuff to this array of stuff todo...'
     ];
     const embedToDo = new EmbedBuilder()
