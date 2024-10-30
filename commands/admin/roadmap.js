c7268d155ae26094d3ae9738545ab4ad93b147af
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-30 17:49:59 -0400

    Update roadmap.js

--- commands/admin/roadmap.js
+++ commands/admin/roadmap.js
@@ -10,6 +10,7 @@ module.exports = {
       'Test changes to guildCreate.js and guildDelete.js',
       'Finish guildCreate.js event telling guild owner how to config bot.',
       'Create guildUpdate.js to update DB entry if guild.name changes.',
+      'Update `/config` and `/system` for DB changes',
       'Do whatever is defined for guildConfig.Part in guildMemberKick.js, guildMemberBan.js, and/or guildMemberDelete.js',
       'Create `/verify` command.',
       'Get the `/bot` command working again so people can get bot info and links to request features and report bugs.',
@@ -17,7 +18,8 @@ module.exports = {
       '\nAdd integration to make this list an issue on GitHub and be able to view/edit all issues?' +
       '\n:arrow_right: <https://docs.github.com/en/rest/quickstart?apiVersion=latest>' +
       '\n:arrow_right: <https://docs.github.com/en/rest/guides/scripting-with-the-rest-api-and-javascript?apiVersion=latest>' +
-      '\n:arrow_right: <https://docs.github.com/en/rest/issues/issues?apiVersion=latest>'
+      '\n:arrow_right: <https://docs.github.com/en/rest/issues/issues?apiVersion=latest>',
+      'Get `/guilds` and `/cipher` working.'
     ];
     const embedToDo = new EmbedBuilder()
       .setTitle( 'Development Roadmap for bot:' )
