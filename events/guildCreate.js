8fe5935ce48041795b444e5c2da273107fb18d5b
Donald J. Fortier II <Technical_13@yahoo.com>
2024-10-31 09:04:45 -0400

    Better link to server in DM.

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -10,20 +10,23 @@ client.on( 'guildCreate', async ( guild ) => {
     const guildOwner = guild.members.cache.get( guild.ownerId );
     const newGuildConfig = await getGuildConfig( guild )
     .then( gotGuild => {
-      console.log( 'gotGuild: %o', gotGuild );
-      console.log( 'I\'ve added %s (id: %s) to my guildDB.', chalk.bold.green( guild.name ), guild.id );
-      /*
-      const chanWidget = ( objGuild.widgetEnabled ? objGuild.widgetChannelId : null );
-      const chanRules = objGuild.rulesChannelId;
-      const chanPublicUpdates = objGuild.publicUpdatesChannelId;
-      const chanSafetyAlerts = objGuild.safetyAlertsChannelId;
-      const chanSystem = objGuild.systemChannelId;
-      const chanFirst = doGuild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
-      const doneConfig = ( guildConfigIds.indexOf( guildId ) != -1 ? true : false );
-      const definedInvite = ( doneConfig ? guildConfigs[ guildConfigIds.indexOf( guildId ) ].Invite : null );
+      if ( !gotGuild.Expires ) {
+      console.log( 'I\'ve added %s (id: %s) to my guildDB.', chalk.bold.green( guild.name ), guild.id ); }
+      else {
+        gotGuild.Expires = null;
+        await guildConfig.updateOne( { _id: guild.id }, gotGuild, { upsert: true } )
+        .then( updateSuccess => { console.log( 'Cleared expriation of DB entry for %s (id: %s) upon re-joining guild.', chalk.bold.green( guild.name ), guild.id ); } )
+        .catch( updateError => { throw new Error( chalk.bold.red.bgYellowBright( 'Error attempting to update %s (id: %s) to clear expiration in DB:\n%o' ), guild.name, guild.id, updateError ); } );
+      }
+      const chanWidget = ( guild.widgetEnabled ? guild.widgetChannelId : null );
+      const chanRules = guild.rulesChannelId;
+      const chanPublicUpdates = guild.publicUpdatesChannelId;
+      const chanSafetyAlerts = guild.safetyAlertsChannelId;
+      const chanSystem = guild.systemChannelId;
+      const chanFirst = guild.channels.cache.filter( chan => { if ( !chan.nsfw && chan.viewable ) { return chan; } } ).first().id;
+      const definedInvite = gotGuild.Invite;
       const chanInvite = ( definedInvite || chanWidget || chanRules || chanPublicUpdates || chanSafetyAlerts || chanSystem || chanFirst );
-      */
-      guildOwner.send( { content: 'Hello! You or someone from https://discord.com/channels/' + guild.id + ' with `ADMINISTRATOR` or `MANAGE_SERVER` permissions has added me to your server!' } )
+      guildOwner.send( { content: 'Hello! You or someone from https://discord.com/channels/' + guild.id + '/' + chanInvite + ' with `ADMINISTRATOR` or `MANAGE_SERVER` permissions has added me to your server!' } )
       .catch( errSendDM => {
         const chanSystem = guild.systemChannelId;
         const chanSafetyAlerts = guild.safetyAlertsChannelId;
@@ -46,13 +49,6 @@ client.on( 'guildCreate', async ( guild ) => {
       console.error( 'Failed to create %s (id: %s) in guildDB on join.', guild.name, guild.id );
       botOwner.send( { content: 'Error adding https://discord.com/channels/' + guild.id + ' to the database.' } );
     } );
-    if ( !newGuildConfig ) { return; }
-    else if ( newGuildConfig.Expires ) {
-      newGuildConfig.Expires = null;
-      await guildConfig.updateOne( { _id: guild.id }, newGuildConfig, { upsert: true } )
-      .then( updateSuccess => { console.log( 'Cleared expriation of DB entry for %s (id: %s) upon joining guild.', chalk.bold.green( guild.name ), guild.id ); } )
-      .catch( updateError => { throw new Error( chalk.bold.red.bgYellowBright( 'Error attempting to update %s (id: %s) to clear expiration in DB:\n%o' ), guild.name, guild.id, updateError ); } );
-    }
   }
   catch ( errObject ) { console.error( 'Uncaught error in %s: %s', chalk.bold.hex( '#FFA500' )( 'guildCreate.js' ), errObject.stack ); }
 } );
