735a345a84697bb873304b62e675c3ae5ebf223f
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-01 16:05:11 -0400

    Tracing guilds thing

--- events/ready.js
+++ events/ready.js
@@ -195,11 +195,11 @@ client.on( 'ready', async rdy => {
         await userConfig.create( newUser )
         .catch( initError => { throw new Error( chalk.bold.red.bgYellowBright( 'Error attempting to add %s (id: %s) to my user database in guildCreate.js:\n%o' ), user.displayName, memberId, initError ); } );
       }
-      let arrUserGuilds =  Array.from( client.guilds.cache.filter( g => g.members.cache.has( userId ) ).keys() ).toSorted();
+      let arrUserGuilds =  Array.from( client.guilds.cache.filter( g => g.members.cache.has( userId ) ).keys() ).toSorted();/* TRON */console.log( 'arrUserGuilds: %o', arrUserGuilds );/* TROFF */
       let currUser = await userConfig.findOne( { _id: userId } );
       let userGuilds = [];
       currUser.Guilds.forEach( ( entry, i ) => { userGuilds.push( entry._id ); } );
-      userGuilds.sort();
+      userGuilds.sort();/* TRON */console.log( 'userGuilds: %o', userGuilds );/* TROFF */
       let newName = ( user.displayName !== currUser.UserName ? true : false );
       let newGuilds = ( arrUserGuilds != userGuilds ? true : false );
       let newVersion = ( verUserDB !== currUser.Version ? true : false );
@@ -210,7 +210,7 @@ client.on( 'ready', async rdy => {
         doUserUpdate = true;
       }
       if ( newGuilds ) {// Update guilds
-        let addedGuilds = arrUserGuilds.filter( a => !userGuilds.includes( a ) );
+        let addedGuilds = arrUserGuilds.filter( a => !userGuilds.includes( a ) );/* TRON */console.log( 'addedGuilds: %o', addedGuilds );/* TROFF */
         if ( addedGuilds.length > 0 ) {// Added guild(s)
           addedGuilds.forEach( async ( guildId ) => {
             let guild = await client.guilds.cache.get( guildId );
@@ -229,7 +229,7 @@ client.on( 'ready', async rdy => {
           } );
           doUserUpdate = true;
         }
-        let removedGuilds = userGuilds.filter( r => !arrUserGuilds.includes( r ) );
+        let removedGuilds = userGuilds.filter( r => !arrUserGuilds.includes( r ) );/* TRON */console.log( 'removedGuilds: %o', removedGuilds );/* TROFF */
         if ( removedGuilds.length > 0 ) {// Removed guild(s)
           let userGuilds = [];
           currUser.Guilds.forEach( ( entry, i ) => { userGuilds.push( entry._id ); } );
