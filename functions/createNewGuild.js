b81ff605a5381642432a620fa54720bd5e49f2cc
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-03 10:24:40 -0500

    Don't try to add what aleady exists...

--- functions/createNewGuild.js
+++ functions/createNewGuild.js
@@ -6,54 +6,57 @@ const verGuildDB = config.verGuildDB;
 
 module.exports = async ( guild ) => {
   try {
-    const botConfig = await getBotConfig();
-    const globalPrefix = ( botConfig.Prefix || config.prefix || '!' );
-    const guildOwner = guild.members.cache.get( guild.ownerId );
-    const newGuildConfig = {
-      _id: guild.id,
-      Bans: [],
-      Blacklist: {
-        Members: [],
-        Roles: []
-      },
-      Commands: [],
-      Expires: null,
-      Guild: {
-        Name: guild.name,
-        Members: guild.members.cache.size,
-        OwnerID: guild.ownerId,
-        OwnerName: guildOwner.displayName
-      },
-      Invite: null,
-      Logs: {
-        Active: true,
-        Chat: null,
-        Default: null,
-        Error: null
-      },
-      Part: {
-        Active: false,
-        Channel: null,
-        Message: null,
-        SaveRoles: true
-      },
-      Prefix: globalPrefix,
-      Premium: true,
-      Version: verGuildDB,
-      Welcome: {
-        Active: false,
-        Channel: null,
-        Message: null,
-        Role: null
-      },
-      Whitelist: {
-        Members: [],
-        Roles: []
-      }
-    };
-    return await guildConfig.create( newGuildConfig )
-    .then( initSuccess => { console.log( chalk.bold.greenBright( 'Succesfully added guild %s (id: %s) to my database.' ), guild.name, guild.id ); return newGuildConfig; } )
-    .catch( initError => { throw new Error( chalk.bold.red.bgYellowBright( `Error attempting to add guild ${guild.name} (id: ${guild.id}) to my database:\n${initError}` ) ); } );
+    if ( await guildConfig.countDocuments( { _id: guild.id } ) === 0 ) {
+      const botConfig = await getBotConfig();
+      const globalPrefix = ( botConfig.Prefix || config.prefix || '!' );
+      const guildOwner = guild.members.cache.get( guild.ownerId );
+      const newGuildConfig = {
+        _id: guild.id,
+        Bans: [],
+        Blacklist: {
+          Members: [],
+          Roles: []
+        },
+        Commands: [],
+        Expires: null,
+        Guild: {
+          Name: guild.name,
+          Members: guild.members.cache.size,
+          OwnerID: guild.ownerId,
+          OwnerName: guildOwner.displayName
+        },
+        Invite: null,
+        Logs: {
+          Active: true,
+          Chat: null,
+          Default: null,
+          Error: null
+        },
+        Part: {
+          Active: false,
+          Channel: null,
+          Message: null,
+          SaveRoles: true
+        },
+        Prefix: globalPrefix,
+        Premium: true,
+        Version: verGuildDB,
+        Welcome: {
+          Active: false,
+          Channel: null,
+          Message: null,
+          Role: null
+        },
+        Whitelist: {
+          Members: [],
+          Roles: []
+        }
+      };
+      return await guildConfig.create( newGuildConfig )
+      .then( initSuccess => { console.log( chalk.bold.greenBright( 'Succesfully added guild %s (id: %s) to my database.' ), guild.name, guild.id ); return newGuildConfig; } )
+      .catch( initError => { throw new Error( chalk.bold.red.bgYellowBright( `Error attempting to add guild ${guild.name} (id: ${guild.id}) to my database:\n${initError}` ) ); } );
+    }
+    else { console.error( 'Guild %s (%s) already exists in my database.', user.id, user.displayName ); }
   }
   catch ( errObject ) { console.error( 'Uncaught error in %s: %s', chalk.hex( '#FFA500' ).bold( 'createNewGuild.js' ), errObject.stack ); }
 };
--- functions/createNewUser.js
+++ functions/createNewUser.js
@@ -5,16 +5,19 @@ const verUserDB = config.verUserDB;
 
 module.exports = async ( user ) => {
   try {
-    const newBotUser = {
-      _id: user.id,
-      Guilds: [],
-      Guildless: null,
-      UserName: user.displayName,
-      Score: 0,
-      Version: verUserDB
+    if ( await userConfig.countDocuments( { _id: user.id } ) === 0 ) {
+      const newBotUser = {
+        _id: user.id,
+        Guilds: [],
+        Guildless: null,
+        UserName: user.displayName,
+        Score: 0,
+        Version: verUserDB
+      }
+      return await userConfig.create( newBotUser )
+      .catch( initError => { throw new Error( chalk.bold.red.bgYellowBright( `Error attempting to add ${user.displayName} (id: ${user.id}) to my user database in createNewUser.js:\n${initError}` ) ); } );
     }
-    return await userConfig.create( newBotUser )
-    .catch( initError => { throw new Error( chalk.bold.red.bgYellowBright( `Error attempting to add ${user.displayName} (id: ${user.id}) to my user database in createNewUser.js:\n${initError}` ) ); } );
+    else { console.error( 'User %s (%s) already exists in my database.', user.id, user.displayName ); }
   }
   catch ( errObject ) { console.error( 'Uncaught error in %s: %s', chalk.hex( '#FFA500' ).bold( 'createNewUser.js' ), errObject.stack ); }
 };
