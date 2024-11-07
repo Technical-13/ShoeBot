2636d684a93495cf1544234a02d5d9aa0c322285
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-07 12:55:04 -0500

    Update createNewUser.js
    Add all new users guilds too...

--- functions/createNewUser.js
+++ functions/createNewUser.js
@@ -1,6 +1,8 @@
+const client = require( '..' );
 const chalk = require( 'chalk' );
 const config = require( '../config.json' );
 const userConfig = require( '../models/BotUser.js' );
+const addUserGuild = require( '../functions/addUserGuild.js' );
 const verUserDB = config.verUserDB;
 
 module.exports = async ( user ) => {
@@ -16,7 +18,16 @@ module.exports = async ( user ) => {
         Version: verUserDB
       }
       return await userConfig.create( newBotUser )
-      .then( updatedUser => { return newBotUser; } )
+      .then( updatedUser => {
+        const botGuilds = client.guilds.cache;
+        let newUserGuilds = ( Array.from( botGuilds.filter( g => g.members.cache.has( user.id ) ).keys() ).toSorted() || [] );
+        for ( let guildId of newUserGuilds ) {// addUserGuild
+          let guild = await botGuilds.get( guildId );
+          if ( botVerbosity >= 1 ) { console.log( '\t\tAdding G:%s to U:%s.', chalk.bold.green( guild.name ), chalk.bold.green( addUser.displayName ) ); }
+          newUser = await addUserGuild( user.id, guild );
+        }
+        return newBotUser;
+      } )
       .catch( initError => { throw new Error( chalk.bold.black.bgCyan( `Error attempting to add ${user.displayName} (id: ${user.id}) to my user database in createNewUser.js:\n` ) + initError ); } );
     }
     else { console.error( 'User %s (%s) already exists in my database.', user.id, user.displayName ); }
