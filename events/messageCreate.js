50bcf84bc20162bae5330981483d8b109be3529b
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-11 10:13:05 -0500

    Update messageCreate.js
    Works in the testers, works in the console... Why isn't it working in the bot...

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -33,7 +33,7 @@ client.on( 'messageCreate', async message => {
     const arrOtherTypeCodes = [ 'GC', 'TB', 'WM', 'GL', 'TL', 'PR', 'BM', 'GT' ];
     const oldRegex = new RegExp('^((?:GC|TB|WM|GL|TL|PR|BM|GT)[a-fA-F0-9]{2,3})', 'g' );
     const newRegex = new RegExp('^((?:GC|TB|WM|GL|TL|PR|BM|GT)[^iIlLoOsSuU\W]{4,7})', 'g' );
-    for ( let word of arrContent ) {/* TRON */console.log( 'word: %o', word );/* TROFF */
+    for ( let word of arrContent ) {/* TRON */console.log( 'word: %o', word );/* TROFF *//* TRON */console.log( 'word.length: %o', word.length );/* TROFF */
       let arrWord = word.trim().match( word.length <= 5 ? oldRegex : newRegex );/* TRON */console.log( 'arrWord: %o', arrWord );/* TROFF */
       let code = ( arrWord ? arrWord[ 0 ].toUpperCase() : ( gcWhitelist.indexOf( word ) != -1 ? word.toUpperCase() : '' ) );/* TRON */console.log( 'code: %o', code );/* TROFF */
       let wordPrefix = code.slice( 0, 2 );/* TRON */console.log( 'wordPrefix: %o', wordPrefix );/* TROFF */
