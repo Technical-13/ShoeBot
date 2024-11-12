58d4404014199622a35308b3fac97ddb1ffc5772
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-12 08:29:09 -0500

    Make this a User command.
    Playing with User commands to learn about different interaction methods.

--- slashCommands/info/ping.js
+++ slashCommands/info/ping.js
@@ -1,15 +1,16 @@
 const { ApplicationCommandType } = require( 'discord.js' );
 const chalk = require( 'chalk' );
+const strScript = chalk.hex( '#FFA500' ).bold( './events/guildMemberAdd.js' );
 
 module.exports = {
   name: 'ping',
-  description: "Check bot's ping.",
-  type: ApplicationCommandType.ChatInput,
+  description: 'Check bot\'s ping.',
+  type: ApplicationCommandType.User,
   cooldown: 1000,
   run: async ( client, interaction ) => {
     try {
-      interaction.reply( { content: `🏓 Pong! Latency: **` + Math.round( client.ws.ping) .toString() + 'ms**', ephemeral: interaction.inGuild() } );
+      interaction.reply( { content: '🏓 Pong! Latency: **' + Math.round( client.ws.ping) .toString() + 'ms**', ephemeral: interaction.inGuild() } );
     }
-    catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', chalk.hex( '#FFA500' ).bold( './slashCommands/info/ping.js' ), errObject.stack ); }
+    catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
   }
 };
