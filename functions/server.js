753e2ca0da1976ea60134c6796270ebfce1ffaa7
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-16 09:02:14 -0500

    Roll most of it back for now until I can figure this out...

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,5 @@
 require( 'dotenv' ).config();
 const ENV = process.env;
-const path = require( 'path' );
 const express = require( 'express' );
 const app = express();
 const bot = ( ENV.BOT_USERNAME || 'Server' );
@@ -8,14 +7,7 @@ const botPort = ( ENV.PORT || 3000 );
 const objTimeString = require( '../jsonObjects/time.json' );
 var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
 
-// Set up Pug as the view engine
-app.set( 'view engine', 'pug' );
-app.set( 'views', path.join( __dirname, 'views' ) );
-
-// Define a route for the home page
-app.get( '/', ( req, res ) => {
-  res.render( 'index', { bot: bot, restartedAt: bot + ' last restarted at ' + strNow() + '.' } );
-} );
+app.all( '/', ( req, res ) => { res.send( bot + ' was last restarted: ' + strNow() ); } );
 
 function keepAlive() {
   var server = app.listen( botPort, () => {
