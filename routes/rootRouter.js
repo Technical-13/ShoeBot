b0da7b283d455a6796e7d56516fa192eee9e35a6
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-16 17:47:03 -0500

    Focus on one thing at a time.

--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -2,8 +2,8 @@ const express = require( 'express' );
 
 const router = express.Router();
 
-router.use( '/auth', require( './auth' ) );
-router.use( '/guilds', require( './guilds' ) );
+//router.use( '/auth', require( './auth' ) );
+//router.use( '/guilds', require( './guilds' ) );
 router.use( '/users', require( './users' ) );
 
 module.exports = router;
