358538ed929d0484b7d0f1b739bf117bf7d904ad
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-16 08:36:10 -0500

    Let's see what I can do with making the web facing page usable with express and Pug

--- functions/server.js
+++ functions/server.js
@@ -1,16 +1,24 @@
 require( 'dotenv' ).config();
 const ENV = process.env;
+const path = require( 'path' );
 const express = require( 'express' );
-const expressServer = express();
+const app = express();
 const bot = ( ENV.BOT_USERNAME || 'Server' );
 const botPort = ( ENV.PORT || 3000 );
 const objTimeString = require( '../jsonObjects/time.json' );
 var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
 
-expressServer.all( '/', ( req, res ) => { res.send( bot + ' was last restarted: ' + strNow() ); } );
+// Set up Pug as the view engine
+app.set( 'view engine', 'pug' );
+app.set( 'views', path.join( __dirname, 'views' ) );
+
+// Define a route for the home page
+app.get( '/', ( req, res ) => {
+  res.render( 'index', { bot: bot, restartedAt: strNow() } );
+} );
 
 function keepAlive() {
-  var server = expressServer.listen( botPort, () => {
+  var server = app.listen( botPort, () => {
     const port = server.address().port;
     console.log( '%s is ready on port %s.', bot, port );
   } );
