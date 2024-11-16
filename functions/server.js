8e11b4e018888601adebca6c250cff36521064ce
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-16 17:04:07 -0500

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -7,19 +7,19 @@ const botPort = ( ENV.PORT || 3000 );
 const objTimeString = require( '../jsonObjects/time.json' );
 var strNow = () => { return ( new Date() ).toLocaleDateString( 'en-us', objTimeString ) };
 
-app.get( '/', ( req, res ) => {
+/*app.get( '/', ( req, res ) => {
   res.send( bot + ' was last restarted: ' + strNow() );
-} );
+} );//*/
 
 app.get( '/users/:userId(\d{17,19})', ( req, res ) => {
   console.log( 'req.params: %o', req.params );
   res.send( 'Hello ' + req.params.userId );
 } );
 
-app.get( '/guilds/:guildId(\d{17,19})', ( req, res ) => {
+/*app.get( '/guilds/:guildId(\d{17,19})', ( req, res ) => {
   console.log( 'req.params: %o', req.params );
   res.send( 'Guild: ' + req.params.guildId );
-} );
+} );//*/
 
 function keepAlive() {
   var server = app.listen( botPort, () => {
