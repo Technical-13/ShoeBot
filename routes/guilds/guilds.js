9eed444e3463b9a7702a71dcc52b842bf6517426
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-16 18:44:36 -0500

    Update guilds.js

--- routes/guilds/guilds.js
+++ routes/guilds/guilds.js
@@ -6,7 +6,7 @@ router.get( '/', ( req, res ) => {
   res.send( 'Looking for a guild?' );
 } );
 
-router.get( '/:guildId', ( req, res ) => {
+router.get( '/:guildId', async ( req, res ) => {
   const { guildId } = req.params;
   const reqGuild = await Guilds.findOne( { _id: guildId } );
   res.send(`Are you looking for ${reqGuild.Guild.Name} with ${new Intl.NumberFormat().format( reqGuild.Guild.Members )} members?`);
