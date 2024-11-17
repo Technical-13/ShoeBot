910c139995ce99f896c7abb506405a1c58408283
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-17 15:25:09 -0500

    Update server.js

--- functions/server.js
+++ functions/server.js
@@ -1,6 +1,7 @@
 require( 'dotenv' ).config();
 const ENV = process.env;
 const chalk = require( 'chalk' );
+const path = require( 'path' );
 const express = require( 'express' );
 const mongoose = require( 'mongoose' );
 mongoose.set( 'strictQuery', false );
@@ -16,9 +17,9 @@ const strScript = chalk.hex( '#FFA500' ).bold( './functions/server.js' );
 
 app.use( cors( { credentials: true, origin: 'https://node4.lunes.host:' + botPort } ) );
 app.use( cookieParser() );
-app.use( express.static( __dirname + '/web/' ) );
 app.use( '/', baseMiddleware );
 app.use( '/', rootRouter );
+app.use( express.static( path.join( __dirname + 'web' ) ) );
 
 if ( !strConnectDB ) { throw new Error( chalk.bold.red( `Failed to get process.env.mongodb in ${strScript}.` ) ); }
 
