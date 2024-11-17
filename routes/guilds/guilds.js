51f04a5ae7035f73045f1c1f364599da7671dae0
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-17 10:17:02 -0500

    Can I get a functioning dropdown selector from /guilds?

--- routes/guilds/guilds.js
+++ routes/guilds/guilds.js
@@ -1,9 +1,22 @@
+require( 'dotenv' ).config();
+const ENV = process.env;
+const bot = ( ENV.BOT_USERNAME || 'Server' );
+const fs = require( 'fs' );
+const cheerio = require( 'cheerio' );
 const express = require( 'express' );
 const router = express.Router();
 const Guilds = require( '../../models/GuildConfig.js' );
+const htmlGuild = fs.readFileSync( './guild.html' );
+const htmlGuilds = fs.readFileSync( './guilds.html' );
 
-router.get( '/', ( req, res ) => {
-  res.send( 'Looking for a guild?' );
+router.get( '/', async ( req, res ) => {
+  const pageGuilds = cheerio.loadBuffer( htmlGuilds );
+  pageGuilds( 'title' ).text( 'Guilds | ' + bot );
+  const allGuilds = await Guilds.find();
+  for ( let dbGuild of allGuilds ) {
+    pageGuilds( '#guild-select' ).append( '<option value="' + dbGuild._id + '">' + dbGuild.Guild.Name + '</option>' );
+  }
+  res.send( pageGuilds );
 } );
 
 router.get( '/:guildId', async ( req, res ) => {
