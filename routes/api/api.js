330cace8e67e38f8a36a15b2db73fa376d8f1e82
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-17 19:47:22 -0500

    Update api.js
    Find partial matches?

--- routes/api/api.js
+++ routes/api/api.js
@@ -11,12 +11,15 @@ router.get( '/g/:guildName', async ( req, res ) => {
   const { guildName } = req.params;
   const reqGuilds = await Guilds.find( { Name: guildName }, '_id Guild:Name', { limit: 10 } );
   console.log( 'reqGuilds: %o', reqGuilds );
+  //res.json( data );
 } );
 
 router.get( '/u/:userName', async ( req, res ) => {
   const { userName } = req.params;
-  const reqUsers = await Users.find( { UserName: userName }, '_id UserName', { limit: 10 } );
+  const regexName = new RegExp( userName, 'i' );
+  const reqUsers = await Users.find( { UserName: regexName }, '_id UserName', { limit: 10 } );
   console.log( 'reqUsers: %o', reqUsers );
+  //res.json( data );
 } );
 
 module.exports = router;
