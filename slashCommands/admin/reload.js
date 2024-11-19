711f4d429de56f1f2676e69f2b3d15012c02ba1b
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-19 13:23:13 -0500

    This is for slashCommands...

--- slashCommands/admin/reload.js
+++ slashCommands/admin/reload.js
@@ -6,10 +6,10 @@ const strScript = chalk.hex( '#FFA500' ).bold( './slashCommands/admin/config.js'
 module.exports = {
   name: 'reload',
   group: 'admin',
-  description: 'Reloads a command.',
+  description: 'Reloads a slashCommand.',
   type: ApplicationCommandType.ChatInput,
   contexts: [ InteractionContextType.Guild, InteractionContextType.BotDM ],
-  options: [ { type: 3, name: 'command', description: 'The command to reload.', required: true } ],
+  options: [ { type: 3, name: 'command', description: 'The name of the slashCommand to reload.', required: true } ],
   cooldown: 1000,
   run: async ( client, interaction ) => {
     await interaction.deferReply( { ephemeral: true } );
@@ -17,23 +17,23 @@ module.exports = {
       const { guild, options, user: author } = interaction;
       const { botOwner, isBotOwner, isBotMod } = await userPerms( author, guild );
       if ( isBotMod && !isBotOwner ) { return interaction.editReply( 'This is currently an **owner only** command.  Please talk to <@' + botOwner.id + '> if you need assistance.' ); }
-      else if ( !isBotOwner ) { return interaction.editReply( 'This is an **owner only** command.  Please talk to <@' + botOwner.id + '> if you need assistance.' ); }
+      else if ( !isBotOwner ) { return interaction.editReply( 'This is an **owner only** command.' ); }
       else {
         const commandName = options.getString( 'command', true ).toLowerCase();
-        const command = client.commands.get( commandName );
+        const command = client.slashCommands.get( commandName );
 
-        if ( !command ) { return interaction.editReply( 'I have no command named `' + commandName + '`!' ); }
+        if ( !command ) { return interaction.editReply( 'I have no slashCommand named `' + commandName + '`!' ); }
 
         delete require.cache[ require.resolve( '../slashCommands/' + command.group + '/' + command.name + '.js' ) ];
 
         const newCommand = require( '../slashCommands/' + command.group + '/' + command.name + '.js' );
-        client.commands.set( newCommand.name, newCommand );
+        client.slashCommands.set( newCommand.name, newCommand );
         await interaction.editReply( 'Command `' + newCommand.name + '` was reloaded!' );
       }
 		}
     catch ( errObject ) {
       console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack );
-      await interaction.editReply( 'There was an error while reloading command `' + command.name + '`:\n`' + errObject.message + '`' );
+      await interaction.editReply( 'There was an error while reloading slashCommand `' + command.name + '`:\n`' + errObject.message + '`' );
 		}
 	},
 };
