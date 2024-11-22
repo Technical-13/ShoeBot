b5c8ad2a97d25e5d6a9e75c4df0d550a2d7b66b5
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-22 15:36:37 -0500

    Start adding authentication to Discord.

---
+++ routes/auth/discord/index.js
@@ -0,0 +1,15 @@
+require( 'dotenv' ).config();
+const ENV = process.env;
+const bot = ( ENV.BOT_USERNAME || 'Server' );
+const fs = require( 'fs' );
+const express = require( 'express' );
+const router = express.Router();
+
+reouter.get( '/signin', ( req, res ) => { res.redirect( 'https://discord.com/oauth2/authorize?client_id=' + ENV.CLIENT_ID + '&response_type=code&redirect_uri=' + uriEncodeComponent( 'http://node4.lunes.host:' + ( ENV.PORT || 3000 ) ) + '&scope=guilds+identify' ); } );
+
+router.get( '/callback',  ( req, res ) => {
+  const { code } = req.query;
+  if ( !code ) { return res.status( 400 ).json( { error: 'Authentication "code" not found in URL parameters.' } ); }
+} );
+
+module.exports = router;
