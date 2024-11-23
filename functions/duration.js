1ad7003036046534f5233d474dfd504366007adf
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-23 13:02:03 -0500

    Update duration.js
    While rare, might want fractional ms time from this.

--- functions/duration.js
+++ functions/duration.js
@@ -2,7 +2,7 @@ const client = require( '..' );
 const chalk = require( 'chalk' );
 const strScript = chalk.hex( '#FFA500' ).bold( './functions/duraction.js' );
 
-module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, getMonths: false, getWeeks: false, getDays: true, getHours: true, getMinutes: true, getSeconds: false }, debug = false ) => {
+module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, getMonths: false, getWeeks: false, getDays: true, getHours: true, getMinutes: true, getSeconds: false, getMs: false }, debug = false ) => {
   try {
     if ( debug ) { console.warn( 'functions/duration.js recieved options: %o', { ms: ms, getUnits: getUnits } ); }
     if ( isNaN( ms ) ) { return '∅ms'; }
@@ -14,44 +14,60 @@ module.exports = async ( ms, getUnits = { getDecades: false, getYears: false, ge
       days: ( typeof getUnits.getDays != 'boolean' ? true : getUnits.getDays ),
       hrs: ( typeof getUnits.getHours != 'boolean' ? true : getUnits.getHours ),
       min: ( typeof getUnits.getMinutes != 'boolean' ? true : getUnits.getMinutes ),
-      secs: ( typeof getUnits.getSeconds != 'boolean' ? false : getUnits.getSeconds )
+      secs: ( typeof getUnits.getSeconds != 'boolean' ? false : getUnits.getSeconds ),
+      ms: ( typeof getUnits.getMs != 'boolean' ? false : getUnits.getMs )
     } );
     if ( debug ) { console.warn( 'functions/duration.js processed options: %o', { ms: ms, objUnits: objUnits } ); }
-    if ( objUnits.xs || objUnits.yrs || objUnits.mos || objUnits.wks || objUnits.days || objUnits.hrs || objUnits.min || objUnits.secs ) {
+    if ( objUnits.xs || objUnits.yrs || objUnits.mos || objUnits.wks || objUnits.days || objUnits.hrs || objUnits.min || objUnits.secs || objUnits.ms ) {
       var intDecades = 0, intYears = 0, intMonths = 0, intWeeks = 0, intDays = 0, intHours = 0, intMinutes = 0, intSeconds = 0;
       var totalSeconds = ( ms / 1000 );
       if ( objUnits.xs ) {
         intDecades = Math.floor( totalSeconds / 315569520 );
         totalSeconds %= 315569520;
+        ms %= 315569520000;
       }
       if ( objUnits.yrs ) {
         intYears = Math.floor( totalSeconds / 31556952 );
         totalSeconds %= 31556952;
+        ms %= 31556952000;
       }
       if ( objUnits.mos ) {
         intMonths = Math.floor( totalSeconds / 2629746 );
         totalSeconds %= 2629746;
+        ms %= 2629746000;
       }
       if ( objUnits.wks && ( intDecades + intYears + intMonths ) === 0 ) {
         intWeeks = Math.floor( totalSeconds / 604800 );
         totalSeconds %= 604800;
+        ms %= 604800000;
       }
       if ( objUnits.days ) {
         intDays = Math.floor( totalSeconds / 86400 );
         totalSeconds %= 86400;
+        ms %= 86400000;
       }
       if ( objUnits.hrs ) {
         intHours = Math.floor( totalSeconds / 3600 );
         totalSeconds %= 3600;
+        ms %= 3600000;
       }
-      if ( objUnits.min ) { intMinutes = Math.floor( totalSeconds / 60 ); }
-      if ( objUnits.secs ) { intSeconds = Math.floor( totalSeconds % 60 ); }
+      if ( objUnits.min ) {
+        intMinutes = Math.floor( totalSeconds / 60 );
+        ms %= 60000;
+      }
+      if ( objUnits.secs ) {
+        intSeconds = Math.floor( totalSeconds % 60 );
+        if ( objUnits.ms ) {
+          intSeconds += ms - ( intSeconds * 1000 );
+        }
+      }
       if ( debug ) {// Display integers figured out above
         const objIntegers = {
           intDecades: intDecades, intYears: intYears,
           intMonths: intMonths, intWeeks: intWeeks, intDays: intDays,
-          intHours: intHours, intMinutes: intMinutes, intSeconds: intSeconds };
-        console.warn( 'functions/duration.js integers: %o', objIntegers );
+          intHours: intHours, intMinutes: intMinutes, intSeconds: intSeconds,
+          intMs: intMs };
+        console.warn( '%s integers: %o', strScript, objIntegers );
       }
 
       const result = [];
